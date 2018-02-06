38710bcdb9b920d24f3a5260d9400ecdd2b08a0f
Vinayaga Sundar <vinayagasundar93@gmail.com>
2018-02-06 12:37:59 +0530

    Issue #2 feat : Added Profile screen for Sunbird

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -5,7 +5,6 @@ import { ResourcesPage } from '../resources/resources';
 import { GroupPage } from '../group/group';
 import { HomePage } from '../home/home';
 import { ProfilePage } from '../profile/profile'
-import { SamplePage } from '../sample/sample';
 
 @Component({
   selector: 'page-tabs',
@@ -17,7 +16,6 @@ export class TabsPage {
   tab2Root = CoursesPage;
   tab3Root = ResourcesPage;
   tab4Root = GroupPage;
-  // tab5Root = SamplePage;
   tab5Root = ProfilePage;
 
   constructor() {
