f6323e9273e953399dc6860f65f48c2f6db94868
shriharsh <shriharshs@gmail.com>
2018-02-15 15:53:47 +0530

    Issue #1 feat: Telemetry Sync added in Data Sync screen

--- src/plugins/settings/datasync/datasync.ts
+++ src/plugins/settings/datasync/datasync.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TelemetryService } from '../../../core/services/telemetry/telemetry.service';
 
 /**
  * Generated class for the DatasyncPage page.
@@ -11,14 +12,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-datasync',
   templateUrl: 'datasync.html',
+  providers: [TelemetryService]
 })
 export class DatasyncPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private telemetryService: TelemetryService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DatasyncPage');
+  }
+
+  onSyncClick() {
+    console.log('Sync called');
+    this.telemetryService.sync();
   }
 
 }
