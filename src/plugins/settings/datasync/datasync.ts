7c8c15fe0278decba10a6e39f7a45c836a643eea
shriharsh <shriharshs@gmail.com>
2018-02-20 18:21:41 +0530

    Issue #1 feat: Data sync options stored locally"

--- src/plugins/settings/datasync/datasync.ts
+++ src/plugins/settings/datasync/datasync.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TelemetryService } from '../../../core/services/telemetry/telemetry.service';
+import { Storage } from "@ionic/storage";
+import { DataSyncType } from "./datasynctype.enum"
 
 /**
  * Generated class for the DatasyncPage page.
@@ -9,23 +11,53 @@ import { TelemetryService } from '../../../core/services/telemetry/telemetry.ser
  * Ionic pages and navigation.
  */
 
+const KEY_DATA_SYNC_TYPE = "data_sync_type";
+
 @Component({
   selector: 'page-datasync',
   templateUrl: 'datasync.html',
   providers: [TelemetryService]
 })
 export class DatasyncPage {
+  dataSyncType: DataSyncType;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private telemetryService: TelemetryService) {
+  OPTIONS: typeof DataSyncType = DataSyncType;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private telemetryService: TelemetryService, private storage: Storage) {
+    this.init();
+  }
+
+  private init() {
+    let that = this;
+    that.storage.get(KEY_DATA_SYNC_TYPE)
+      .then(val => {
+        if (val === undefined || val === "" || val === null) {
+          return DataSyncType.off
+        } else {
+          return val
+        }
+      })
+      .then(val => {
+        this.dataSyncType = val
+
+        console.error("default value - " + this.dataSyncType)
+      })
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DatasyncPage');
   }
 
+  onSelected() {
+    console.log("Value - " + this.dataSyncType)
+    this.storage.set(KEY_DATA_SYNC_TYPE, this.dataSyncType)
+  }
+
   onSyncClick() {
     console.log('Sync called');
     this.telemetryService.sync();
   }
+
+
 
 }
