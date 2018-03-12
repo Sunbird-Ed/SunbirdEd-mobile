6afc22ba3930e2445915a009272ce83e6519aa03
Vinayaga Sundar <vinayagasundar93@gmail.com>
2018-03-12 14:47:52 +0530

    Issue #5 feat: Added Horizotnal Scroll and Custom Course card view

--- src/pages/courses/courses.module.ts
+++ src/pages/courses/courses.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CoursesPage } from './courses';
 import { TranslateModule } from '@ngx-translate/core';
+import { CourseCard } from '../../component/card/course/course-card';
+import { Ionic2RatingModule } from "ionic2-rating";
 
 @NgModule({
   declarations: [
     CoursesPage,
+    CourseCard
   ],
   imports: [
     IonicPageModule.forChild(CoursesPage),
-    TranslateModule.forChild()
+    TranslateModule.forChild(),
+    Ionic2RatingModule
   ],
   exports: [
     CoursesPage
