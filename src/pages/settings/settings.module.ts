3606adb5522cdbdd32d4fb74a5dbdb5a17479ade
Vivek kasture <vivek_kasture@techjoomla.com>
2018-03-12 15:06:14 +0530

    Issue #4 feat: Removed LnaguageSettingsPage dependency

--- src/pages/settings/settings.module.ts
+++ src/pages/settings/settings.module.ts
@@ -7,13 +7,12 @@ import { AboutappPage } from './aboutapp/aboutapp';
 import { TermsofservicePage } from './termsofservice/termsofservice';
 import { PrivacypolicyPage } from './privacypolicy/privacypolicy';
 import { TranslateModule } from '@ngx-translate/core';
-import { LanguageSettingsPage } from '../language-settings/language-settings';
 import { OnboardingPageModule } from '../onboarding/onboarding.module';
 import { LanguageSettingsPageModule } from '../language-settings/language-settings.module';
 
 @NgModule({
-	declarations: [SettingsPage, LanguageSettingsPage, DatasyncPage, AboutusPage, AboutappPage, PrivacypolicyPage, TermsofservicePage],
-	entryComponents: [LanguageSettingsPage, DatasyncPage, AboutusPage, AboutappPage, PrivacypolicyPage, TermsofservicePage],
+	declarations: [SettingsPage, DatasyncPage, AboutusPage, AboutappPage, PrivacypolicyPage, TermsofservicePage],
+	entryComponents: [DatasyncPage, AboutusPage, AboutappPage, PrivacypolicyPage, TermsofservicePage],
 	imports: [
 		OnboardingPageModule,
 		LanguageSettingsPageModule,
