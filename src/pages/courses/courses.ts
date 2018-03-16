71f028152cd60f345bb8e8951d1d73c42cccf49a
Nilesh More <nilesh_m@tekditechnologies.com>
2018-03-16 15:35:27 +0530

    Issue #5 fix: fixed issues

--- src/pages/courses/courses.ts
+++ src/pages/courses/courses.ts
@@ -1,13 +1,15 @@
-import { CourseCard } from './../../component/card/course/course-card';
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+import { CourseService, AuthService, EnrolledCoursesRequest } from 'sunbird';
+import { CourseCard } from './../../component/card/course/course-card';
 
 @IonicPage()
 @Component({
   selector: 'page-courses',
-  templateUrl: 'courses.html'
+  templateUrl: 'courses.html',
+  providers:[CourseService, AuthService]
 })
 export class CoursesPage implements OnInit {
 
@@ -27,9 +29,24 @@ export class CoursesPage implements OnInit {
   userId: string;
 
   /**
+   * Flag to show/hide loader
+   */
+  showLoader: boolean;
+
+  /**
    * Http service to make api call
    */
   public http: HttpClient;
+
+  /**
+   * Course service to get enrolled courses
+   */
+  public courseService: CourseService;
+
+  /**
+   * Auth service to get user id
+   */
+  public authService: AuthService;
 
   /**
    * Default method of class CoursesPage
@@ -37,8 +54,10 @@ export class CoursesPage implements OnInit {
    * @param {NavController} navCtrl Reference of nav controller to navigate user from one page to another
    * @param {HttpClient} http Reference of http client service to make api call
    */
-  constructor(public navCtrl: NavController, http: HttpClient) {
+  constructor(public navCtrl: NavController, http: HttpClient, courseService: CourseService, authService: AuthService) {
     this.http = http;
+    this.courseService = courseService;
+    this.authService = authService;
     // TODO: remove this hardcodec id before pushing the code
     this.userId = '659b011a-06ec-4107-84ad-955e16b0a48a';
   }
@@ -50,12 +69,15 @@ export class CoursesPage implements OnInit {
    */
   getEnrolledCourses(): void {
     console.log('fetching enrolled course...');
+    this.spinner(true);
     this.http.get('http://www.mocky.io/v2/5aa8ebdb3200003f22165980').subscribe(
       (data: any) => {
         this.enrolledCourse = data.courses;
+        this.spinner(false);
       },
       (error: any) => {
         console.log('error while fetching enrolled courses');
+        this.spinner(false);
       }
     );
   }
@@ -66,16 +88,25 @@ export class CoursesPage implements OnInit {
    * It internally calls course handler of genie sdk
    */
   getPopularAndLatestCourses(): void {
+    this.spinner(true);
     this.http.get('http://www.mocky.io/v2/5aa9ff1c330000ba092da65a').subscribe(
       (data: any) => {
         this.popularAndLatestCourses = data.result.response.sections ? data.result.response.sections : [];
+        this.spinner(false);
       },
       (error: any) => {
         console.log('error while fetching popular courses');
+        this.spinner(false);
       }
     );
   }
 
+  /**
+   * To start and stop loader
+   */
+  spinner(flag: boolean){
+    this.showLoader = flag;
+  }
   /**
    * Angular life cycle hooks
    */
