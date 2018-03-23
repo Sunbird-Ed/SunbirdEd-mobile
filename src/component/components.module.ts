be9f43be54d4b0dc9b765908e04a76104a1beb80
souvikmondal <souvik.mondal@tarento.com>
2018-03-23 12:53:11 +0530

    Issue #0000 feat: Implemented library page

--- src/component/components.module.ts
+++ src/component/components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { CourseCard } from "./card/course/course-card";
 import { IonicPageModule } from 'ionic-angular';
 import { Ionic2RatingModule } from "ionic2-rating";
+import { IonicImageLoader } from "ionic-image-loader";
 
 @NgModule({
     declarations: [
@@ -9,7 +10,8 @@ import { Ionic2RatingModule } from "ionic2-rating";
     ],
     imports: [
         IonicPageModule.forChild(CourseCard),
-        Ionic2RatingModule
+        Ionic2RatingModule,
+        IonicImageLoader
     ],
     exports: [
         CourseCard
