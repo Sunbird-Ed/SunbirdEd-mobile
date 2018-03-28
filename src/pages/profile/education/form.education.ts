038312fe33b0e58ce92600d66db429fb9f31415d
Vivek kasture <vivek_kasture@techjoomla.com>
2018-03-28 12:18:34 +0530

    Issue #7 feat: Added missing fields Navigation

--- src/pages/profile/education/form.education.ts
+++ src/pages/profile/education/form.education.ts
@@ -12,13 +12,21 @@ export class FormEducation implements OnInit{
   tabBarElement: any;
   isNewForm: boolean = true;
   educationForm: FormGroup;
-  formDetails: any = [];
+  formDetails: any = {};
   constructor(public navCtrl: NavController, public fb: FormBuilder, public navParams: NavParams) {
     //Need to hide bottom tab
     this.tabBarElement = document.querySelector('.tabbar.show-tabbar');
-    this.isNewForm = this.navParams.get('addForm');
-    this.formDetails = this.navParams.get('formDetails');
-    
+    this.isNewForm = this.navParams.get('addForm') || true;
+    this.formDetails = this.navParams.get('formDetails') || {};
+
+    this.educationForm = this.fb.group({
+      degree: [(this.formDetails.degree) ? this.formDetails.degree : '', Validators.required],
+      instituteName: [(this.formDetails.name) ? this.formDetails.name : '', Validators.required],
+      yop: [(this.formDetails.yearOfPassing) ? this.formDetails.yearOfPassing : ''],
+      percentage: [(this.formDetails.percentage) ? this.formDetails.percentage : ''],
+      grade: [(this.formDetails.grade) ? this.formDetails.grade : ''],
+      board: [(this.formDetails.boardOrUniversity) ? this.formDetails.boardOrUniversity : '']
+    });
   }
 
   ionViewWillEnter() {
@@ -30,14 +38,6 @@ export class FormEducation implements OnInit{
   }
 
   ngOnInit() {
-    this.educationForm = this.fb.group({
-      degree: [(this.formDetails.degree) ? this.formDetails.degree : '', Validators.required],
-      instituteName: [(this.formDetails.name) ? this.formDetails.name : '', Validators.required],
-      yop: [(this.formDetails.yearOfPassing) ? this.formDetails.yearOfPassing : ''],
-      percentage: [(this.formDetails.percentage) ? this.formDetails.percentage : ''],
-      grade: [(this.formDetails.grade) ? this.formDetails.grade : ''],
-      board: [(this.formDetails.boardOrUniversity) ? this.formDetails.boardOrUniversity : '']
-    });
   }
 
   onSubmit(event) {
