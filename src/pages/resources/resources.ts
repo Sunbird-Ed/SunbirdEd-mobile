352373946f3ab87b60267628d4d299db7e2fcadf
indraja <machani.indraja@gmail.com>
2018-04-16 17:15:09 +0530

    Issue #20 feat:Library page-Adding onboarding screens

--- src/pages/resources/resources.ts
+++ src/pages/resources/resources.ts
@@ -1,7 +1,8 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, NgZone, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PageAssembleService, PageAssembleCriteria, ContentService, AuthService } from "sunbird";
 import * as _ from 'lodash';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-resources',
@@ -17,6 +18,10 @@ export class ResourcesPage {
   localResources: Array<any>;
 
   userId: string;
+  onBoardingSlides: any[];
+  @ViewChild(Slides) mSlides: Slides;
+  selectedOptions: any;
+
   /**
    * Contains reference of content service
    */
@@ -31,6 +36,66 @@ export class ResourcesPage {
     contentService: ContentService, authService: AuthService) {
     this.contentService = contentService;
     this.authService = authService;
+
+    this.selectedOptions = [];
+
+    this.onBoardingSlides = [
+      {
+        'title': 'Which board does your school follow?',
+        'desc': 'SELECT BOARD',
+        'options': [
+          { text: 'Board1', value: 'board1' },
+          { text: 'Board2', value: 'board2' },
+          { text: 'Board3', value: 'board3' },
+          { text: 'Board4', value: 'board4' }
+        ]
+      },
+      {
+        'title': 'Which class do you belong to?',
+        'desc': 'SELECT CLASS',
+        'options': [
+          { text: 'Board1', value: 'board1' },
+          { text: 'Board2', value: 'board2' },
+          { text: 'Board3', value: 'board3' },
+          { text: 'Board4', value: 'board4' }
+        ]
+      },
+      {
+        'title': 'Which subjects are you looking for?',
+        'desc': 'SELECT SUBJECT',
+        'options': [
+          { text: 'Board1', value: 'board1' },
+          { text: 'Board2', value: 'board2' },
+          { text: 'Board3', value: 'board3' },
+          { text: 'Board4', value: 'board4' }
+        ]
+      },
+      {
+        'title': 'What medium/language does your school teach in?',
+        'desc': 'SELECT MEDIUM/LANG',
+        'options': [
+          { text: 'Board1', value: 'board1' },
+          { text: 'Board2', value: 'board2' },
+          { text: 'Board3', value: 'board3' },
+          { text: 'Board4', value: 'board4' }
+        ]
+      }
+    ]
+  }
+
+  onSlideDrag() {
+    let currentIndex = this.mSlides.getActiveIndex();
+    console.log('Current index is', currentIndex);
+    console.log(this.selectedOptions.length);
+    console.log(this.selectedOptions[currentIndex]);
+    // let lockSwipeToNext = !(this.pets.length && this.pets[currentIndex] && this.pets[currentIndex].length);
+    this.mSlides.lockSwipeToNext(!(this.selectedOptions.length && this.selectedOptions[currentIndex]
+      && this.selectedOptions[currentIndex].length));
+  }
+
+  handleOnBoardingOptionSelected(index: number) {
+    console.log("index: " + index + ", selectedOptions " + this.selectedOptions[index]);
+    // slides.
   }
 
   ionViewWillEnter() {
@@ -46,7 +111,7 @@ export class ResourcesPage {
       data = JSON.parse(data);
       console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAA', data);
       this.ngZone.run(() => {
-         // = data.result ? data.result : [];
+        // = data.result ? data.result : [];
         if (data.result) {
           _.forEach(data.result, (value, key) => {
             value.contentData.lastUpdatedOn = value.lastUpdatedTime;
