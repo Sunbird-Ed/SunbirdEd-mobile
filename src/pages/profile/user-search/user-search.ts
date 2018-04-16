66c320e19ecbe4d1f7f1accf65bf0b7fdc990090
Vivek kasture <vivek_kasture@techjoomla.com>
2018-04-17 01:27:31 +0530

    Issue #24 feat: User Search Design Changes

--- src/pages/profile/user-search/user-search.ts
+++ src/pages/profile/user-search/user-search.ts
@@ -1,6 +1,7 @@
 import { Component, NgZone, ViewChild, Input } from "@angular/core";
 import { AuthService, UserProfileService } from "sunbird";
-import { NavController, NavParams } from "ionic-angular";
+import { NavController, NavParams, LoadingController } from "ionic-angular";
+import { Renderer } from '@angular/core';
 
 import { ProfilePage } from "./../profile";
 
@@ -11,7 +12,7 @@ import { ProfilePage } from "./../profile";
 
 /* This component shows the list of user based on search*/
 export class UserSearchComponent {
-
+  @ViewChild('input') input;
   searchInput: string = "";
   userList: any = [];
   fallBackImage: string = "./assets/imgs/ic_profile_default.png";
@@ -28,14 +29,20 @@ export class UserSearchComponent {
     public navParams: NavParams,
     private authService: AuthService,
     private userService: UserProfileService,
-    private zone: NgZone
+    private zone: NgZone,
+    private renderer: Renderer,
+    private loadingCtrl: LoadingController,
   ) { }
 
   /**
    * Makes an search user API call
    * @param {object} scrollEvent - infinite Scroll Event
    */
-  onInput(scrollEvent = undefined): void {
+  onInput(event, scrollEvent = undefined): void {
+    let loader = this.getLoader();
+
+    if(!this.enableInfiniteScroll) loader.present();
+    if(event) this.renderer.invokeElementMethod(event.target, 'blur');
     this.authService.getSessionData(session => {
       if (session === undefined || session == null) {
         console.error("session is null");
@@ -50,6 +57,7 @@ export class UserSearchComponent {
         };
         if (req.query == "") {
           this.userList = [];
+          loader.dismiss();
         } else {
           this.userService.searchUser(req,
             (res: any) => {
@@ -57,11 +65,13 @@ export class UserSearchComponent {
                 Array.prototype.push.apply(this.userList, JSON.parse(JSON.parse(res).searchUser).content)
                 this.enableInfiniteScroll = (this.apiOffset + this.apiLimit) < JSON.parse(JSON.parse(res).searchUser).count ? true : false;
                 if (scrollEvent) scrollEvent.complete();
+                loader.dismiss();
               });
             },
             (error: any) => {
               console.error("Error", error);
               if (scrollEvent) scrollEvent.complete();
+              loader.dismiss();
             }
           );
         }
@@ -88,9 +98,22 @@ export class UserSearchComponent {
   doInfiniteScroll(scrollEvent): void {
     if (this.enableInfiniteScroll) {
       this.apiOffset += this.apiLimit;
-      this.onInput(scrollEvent);
+      this.onInput(undefined, scrollEvent);
     } else {
       scrollEvent.complete();
     }
+  }
+
+  ionViewDidEnter() {
+    setTimeout(()=>{
+      this.input.setFocus();
+    }, 100);
+  }
+
+  getLoader(): any {
+    return this.loadingCtrl.create({
+      duration: 30000,
+      spinner: "crescent"
+    });
   }
 }
