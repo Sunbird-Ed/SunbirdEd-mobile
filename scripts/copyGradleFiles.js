0965e323329ebc3a2317e7f3607bfe2d6b4a6a50
VinayagaSundar <vinayagasundar93@gmail.com>
2018-05-09 12:51:20 +0530

    Issue #SB-3044 chore: read gradle values from properties files

--- scripts/copyGradleFiles.js
+++ scripts/copyGradleFiles.js
@@ -1,5 +1,5 @@
 module.exports = function (context) {
     var fs = require('fs');
     fs.copyFileSync("buildConfig/build-extras.gradle", "platforms/android/app/build-extras.gradle");
-    fs.copyFileSync("buildConfig/secret.gradle", "platforms/android/app/secret.gradle");
+    fs.copyFileSync("buildConfig/sunbird.properties", "platforms/android/gradle.properties");
 }
