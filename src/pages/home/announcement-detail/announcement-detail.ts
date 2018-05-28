d5defa7fe7e4ff5915bd04d103cd2ffd0b72c2c5
Guru123 <gurudayal2k9@gmail.com>
2018-05-28 18:33:08 +0530

    Issue #SB-3044 fix: deleted unused images  from assets.

--- src/pages/home/announcement-detail/announcement-detail.ts
+++ src/pages/home/announcement-detail/announcement-detail.ts
@@ -62,7 +62,7 @@ export class AnnouncementDetailComponent implements OnInit {
 
     /**
      * Visibility of the progress bar
-     * 
+     *
      */
     public showProgressBar: boolean = false;
 
