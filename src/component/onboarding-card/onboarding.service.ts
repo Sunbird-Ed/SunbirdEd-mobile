25e27730adc2a11008e8550c90964953ce2b25fa
Shriharsh <shriharshs@gmail.com>
2018-06-04 15:12:06 +0530

    Issue #SB-3836 feat: Onboarding Syllabus card and Guest Edit profile Syllabus added

--- src/component/onboarding-card/onboarding.service.ts
+++ src/component/onboarding-card/onboarding.service.ts
@@ -6,7 +6,10 @@ import {
     CategoryRequest,
     FrameworkDetailsRequest,
     ProfileService,
-    Profile
+    Profile,
+    SharedPreferences,
+    FormRequest,
+    FormService
 } from 'sunbird';
 
 @Injectable()
@@ -16,79 +19,117 @@ export class OnboardingService {
     onBoardingSlides: any[];
     isOnBoardingCardCompleted: boolean = false;
     currentIndex: number = 0;
-
+    selectedLanguage: string;
     categories: Array<any> = [];
+    syllabusList: Array<any> = [
+        // {
+        //     'checked': false,
+        //     'text': 'Syllabus 1',
+        //     'value': 'sylllabus1',
+        // },
+        // {
+        //     'checked': false,
+        //     'text': 'Syllabus 2',
+        //     'value': 'sylllabus2',
+        // },
+        // {
+        //     'checked': false,
+        //     'text': 'Syllabus 3',
+        //     'value': 'sylllabus3',
+        // }
+    ];
     boardList: Array<string> = [];
     gradeList: Array<string> = [];
     subjectList: Array<string> = [];
     mediumList: Array<string> = [];
+    frameworks: Array<any> = [];
 
     constructor(
         private framework: FrameworkService,
         private profileService: ProfileService,
         public events: Events,
-        public zone: NgZone
-    ) { }
+        public zone: NgZone,
+        private preference: SharedPreferences,
+        private formService: FormService
+    ) {
+
+        //fetch language code
+        this.preference.getString('selected_language_code', (val: string) => {
+            if (val && val.length) {
+                this.selectedLanguage = val;
+            }
+        });
+
+    }
 
     initializeCard(): Promise<any> {
         return new Promise((resolve, reject) => {
             this.getFrameworkDetails()
-            .then(catagories => {
-                this.categories = catagories;
-                this.initializeSlides();
-                return this.getCurrentUser();
-            })
-            .then(index => {
-                resolve(index);
-            })
-            .catch(err => {
-                reject(err);
-            });
+                .then(catagories => {
+                    this.categories = catagories;
+                    this.initializeSlides();
+                    return this.getCurrentUser();
+                })
+                .then(index => {
+                    resolve(index);
+                })
+                .catch(err => {
+                    reject(err);
+                });
         });
     }
 
     initializeSlides() {
         this.onBoardingSlides = [
-                {
-                    'id': 'boardList',
-                    'title': 'BOARD_QUESTION',
-                    'desc': 'BOARD_OPTION_TEXT',
-                    'options': [],
-                    'selectedOptions': '',
-                    'selectedCode': []
-                },
-                {
-                    'id': 'gradeList',
-                    'title': 'GRADE_QUESTION',
-                    'desc': 'GRADE_OPTION_TEXT',
-                    'options': [],
-                    'selectedOptions': '',
-                    'selectedCode': []
-                },
-                {
-                    'id': 'subjectList',
-                    'title': 'SUBJECT_QUESTION',
-                    'desc': 'SUBJECT_OPTION_TEXT',
-                    'options': [],
-                    'selectedOptions': '',
-                    'selectedCode': []
-                },
-                {
-                    'id': 'mediumList',
-                    'title': 'MEDIUM_QUESTION',
-                    'desc': 'MEDIUM_OPTION_TEXT',
-                    'options': [],
-                    'selectedOptions': '',
-                    'selectedCode': []
-                }
-            ];
-            console.log("Initialized", this.onBoardingSlides);
+            {
+                'id': 'syllabusList',
+                'title': 'SYLLABUS_QUESTION',
+                'desc': 'SYLLABUS_OPTION_TEXT',
+                'options': [],
+                'selectedOptions': '',
+                'selectedCode': []
+            },
+            {
+                'id': 'boardList',
+                'title': 'BOARD_QUESTION',
+                'desc': 'BOARD_OPTION_TEXT',
+                'options': [],
+                'selectedOptions': '',
+                'selectedCode': []
+            },
+            {
+                'id': 'gradeList',
+                'title': 'GRADE_QUESTION',
+                'desc': 'GRADE_OPTION_TEXT',
+                'options': [],
+                'selectedOptions': '',
+                'selectedCode': []
+            },
+            {
+                'id': 'subjectList',
+                'title': 'SUBJECT_QUESTION',
+                'desc': 'SUBJECT_OPTION_TEXT',
+                'options': [],
+                'selectedOptions': '',
+                'selectedCode': []
+            },
+            {
+                'id': 'mediumList',
+                'title': 'MEDIUM_QUESTION',
+                'desc': 'MEDIUM_OPTION_TEXT',
+                'options': [],
+                'selectedOptions': '',
+                'selectedCode': []
+            }
+        ];
 
+        this.onBoardingSlides[0].options = this.syllabusList;
+        this.onBoardingSlides[1].options = this.boardList;
+        this.onBoardingSlides[2].options = this.gradeList;
+        this.onBoardingSlides[3].options = this.subjectList;
+        this.onBoardingSlides[4].options = this.mediumList;
 
-        this.onBoardingSlides[0].options = this.boardList;
-        this.onBoardingSlides[1].options = this.gradeList;
-        this.onBoardingSlides[2].options = this.subjectList;
-        this.onBoardingSlides[3].options = this.mediumList;
+        console.log("Initialized", this.onBoardingSlides);
     }
     /**
      * Method user to fetch Current Guest User
@@ -100,36 +141,51 @@ export class OnboardingService {
                 let index = 0;
                 this.profile = JSON.parse(res);
                 this.currentIndex = 0;
+                if (this.profile.syllabus && this.profile.syllabus[0] !== '') {
+                    //this.onBoardingSlides[0].selectedOptions = this.getDisplayValues(0, this.profile.syllabus);
+                    let displayValues = [];
+
+                    this.syllabusList.forEach(element => {
+                        if (_.includes(this.profile.syllabus, element.value)) {
+                            displayValues.push(element.text);
+                        }
+                    });
+                    this.onBoardingSlides[0].selectedOptions = this.arrayToString(displayValues);
+
+                    // this.onBoardingSlides[0].selectedOptions = this.profile.syllabus[0];
+                    this.currentIndex = 20;
+                    index = 1;
+                }
                 if (this.profile.board && this.profile.board[0] !== '') {
                     console.log("Categories", this.categories);
-                    this.onBoardingSlides[0].selectedOptions = this.getDisplayValues(0, this.profile.board);
-                    this.currentIndex = 25;
-                    index = 1;
+                    this.onBoardingSlides[1].selectedOptions = this.getDisplayValues(0, this.profile.board);
+                    this.currentIndex = 40;
+                    index = 2;
                 }
                 if (this.profile.grade && this.profile.grade[0] !== '') {
                     //this.onBoardingSlides[1].selectedOptions = this.profile.grade;
-                    this.onBoardingSlides[1].selectedOptions = this.getDisplayValues(1, this.profile.grade);
-                    this.currentIndex = 50;
-                    index = 2;
+                    this.onBoardingSlides[2].selectedOptions = this.getDisplayValues(1, this.profile.grade);
+                    this.currentIndex = 60;
+                    index = 3;
                 }
                 if (this.profile.subject && this.profile.subject[0] !== '') {
                     //this.onBoardingSlides[2].selectedOptions = this.profile.subject;
-                    this.onBoardingSlides[2].selectedOptions = this.getDisplayValues(2, this.profile.subject);
-                    this.currentIndex = 75;
-                    index = 3;
+                    this.onBoardingSlides[3].selectedOptions = this.getDisplayValues(2, this.profile.subject);
+                    this.currentIndex = 80;
+                    index = 4;
                 }
                 if (this.profile.medium && this.profile.medium[0] !== '') {
                     //this.onBoardingSlides[3].selectedOptions = this.profile.medium;
-                    this.onBoardingSlides[3].selectedOptions = this.getDisplayValues(3, this.profile.medium);
+                    this.onBoardingSlides[4].selectedOptions = this.getDisplayValues(3, this.profile.medium);
                     this.currentIndex = 100;
-                    index = 4;
+                    index = 5;
                 }
                 resolve(index);
             },
-            (err: any) => {
-                console.log("Err1", err);
-                reject(err);
-            });
+                (err: any) => {
+                    console.log("Err1", err);
+                    reject(err);
+                });
         });
     }
 
@@ -146,12 +202,17 @@ export class OnboardingService {
     /**
      * It fetches all the categories using Framework API
      */
-    getFrameworkDetails(): Promise<any> {
+    getFrameworkDetails(frameworkId?: string): Promise<any> {
 
         return new Promise((resolve, reject) => {
             let req: FrameworkDetailsRequest = {
                 defaultFrameworkDetails: true
             };
+
+            if (frameworkId !== undefined && frameworkId.length) {
+                req.defaultFrameworkDetails = false;
+                req.frameworkId = frameworkId[0];
+            }
 
             this.framework.getFrameworkDetails(req,
                 (res: any) => {
@@ -180,16 +241,16 @@ export class OnboardingService {
                 let value = {};
                 resposneArray.forEach(element => {
                     if (list === "boardList" && this.profile.board && this.profile.board.length && this.profile.board.indexOf(element.code) > -1) {
-                        this.onBoardingSlides[0].selectedCode.push(element.code);
-                        value = { 'text': element.name, 'value': element.code, 'checked': true };
-                    } else if (list === "gradeList" && this.profile.grade && this.profile.grade.length && this.profile.grade.indexOf(element.code) > -1) {
                         this.onBoardingSlides[1].selectedCode.push(element.code);
                         value = { 'text': element.name, 'value': element.code, 'checked': true };
-                    } else if (list === "subjectList" && this.profile.subject && this.profile.subject.length && this.profile.subject.indexOf(element.code) > -1) {
+                    } else if (list === "gradeList" && this.profile.grade && this.profile.grade.length && this.profile.grade.indexOf(element.code) > -1) {
                         this.onBoardingSlides[2].selectedCode.push(element.code);
                         value = { 'text': element.name, 'value': element.code, 'checked': true };
-                    } else if (list === "mediumList" && this.profile.medium && this.profile.medium.length && this.profile.medium.indexOf(element.code) > -1) {
+                    } else if (list === "subjectList" && this.profile.subject && this.profile.subject.length && this.profile.subject.indexOf(element.code) > -1) {
                         this.onBoardingSlides[3].selectedCode.push(element.code);
+                        value = { 'text': element.name, 'value': element.code, 'checked': true };
+                    } else if (list === "mediumList" && this.profile.medium && this.profile.medium.length && this.profile.medium.indexOf(element.code) > -1) {
+                        this.onBoardingSlides[4].selectedCode.push(element.code);
                         value = { 'text': element.name, 'value': element.code, 'checked': true };
                     } else {
                         value = { 'text': element.name, 'value': element.code, 'checked': false };
@@ -197,8 +258,8 @@ export class OnboardingService {
 
                     this[list].push(value);
                 });
-                if(list !== 'gradeList') {
-                    this[list] =  _.orderBy(this[list], ['text'], ['asc']);
+                if (list !== 'gradeList') {
+                    this[list] = _.orderBy(this[list], ['text'], ['asc']);
                 }
 
                 this.getListArray(list);
@@ -210,6 +271,37 @@ export class OnboardingService {
         //}
     }
 
+    getSyllabusDetails() {
+        let req: FormRequest = {
+            type: 'user',
+            subType: 'instructor',
+            action: 'onboarding',
+        };
+
+        this.formService.getForm(req,
+            (res: any) => {
+                let response: any = JSON.parse(res);
+                console.log("Form Result - " + response.result);
+                let fields: Array<any> = response.result.fields;
+
+                fields.forEach(field => {
+                    if (field.language === this.selectedLanguage) {
+                        this.frameworks = field.range;
+                    }
+                });
+
+                if (this.frameworks != null && this.frameworks.length > 0) {
+                    this.frameworks.forEach(frameworkDetails => {
+                        let value = { 'text': frameworkDetails.name, 'value': frameworkDetails.frameworkId, 'checked': false };
+                        this.syllabusList.push(value);
+                    });
+                }
+            },
+            (error: any) => {
+                console.log("Error - " + error);
+            })
+    }
+
     /**
      * It checks whether Previous Field is filled or not
      * @param {number} index
@@ -218,19 +310,44 @@ export class OnboardingService {
      */
     checkPrevValue(index: number = 0, currentField, prevSelectedValue = []) {
 
-        if (index != 0) {
+        if (index === 0) {
+            this[currentField] = this.syllabusList;
+        } else if (index === 1) {
+            this.getFrameworkDetails(prevSelectedValue[0])
+                .then(catagories => {
+                    this.categories = catagories;
+                });
+
             let request: CategoryRequest = {
-                currentCategory: this.categories[index].code,
-                prevCategory: this.categories[index - 1].code,
-                selectedCode: prevSelectedValue
+                currentCategory: this.categories[0].code,
+                // prevCategory: ,
+                // selectedCode: prevSelectedValue
             }
             this.getCategoryData(request, currentField);
         } else {
             let request: CategoryRequest = {
-                currentCategory: this.categories[index].code
+                currentCategory: this.categories[index - 1].code,
+                prevCategory: this.categories[index - 2].code,
+                selectedCode: prevSelectedValue
             }
             this.getCategoryData(request, currentField);
         }
+
+        // if (index != 0) {
+        //     let request: CategoryRequest = {
+        //         currentCategory: this.categories[index].code,
+        //         prevCategory: this.categories[index - 1].code,
+        //         selectedCode: prevSelectedValue
+        //     }
+        //     this.getCategoryData(request, currentField);
+        // } else {
+        //     // let request: CategoryRequest = {
+        //     //     currentCategory: this.categories[index].code
+        //     // }
+        //     // this.getCategoryData(request, currentField);
+        //     this[currentField] = this.syllabusList;
+
+        // }
     }
 
     /**
@@ -240,12 +357,14 @@ export class OnboardingService {
      */
     getListName(index: number): string {
         if (index == 0) {
-            return 'boardList';
+            return 'syllabusList';
         } else if (index == 1) {
-            return 'gradeList';
+            return 'boardList';
         } else if (index == 2) {
-            return 'subjectList';
+            return 'gradeList';
         } else if (index == 3) {
+            return 'subjectList';
+        } else if (index == 4) {
             return 'mediumList';
         } else {
             return 'boardList';
@@ -257,14 +376,16 @@ export class OnboardingService {
      * @param {string} name name of the local variable
      */
     getListArray(name: string): void {
-        if (name == 'boardList') {
-            this.onBoardingSlides[0].options = this.boardList;
+        if (name == 'syllabusList') {
+            this.onBoardingSlides[0].options = this.syllabusList;
+        } else if (name == 'boardList') {
+            this.onBoardingSlides[1].options = this.boardList;
         } else if (name == 'gradeList') {
-            this.onBoardingSlides[1].options = this.gradeList;
+            this.onBoardingSlides[2].options = this.gradeList;
         } else if (name == 'subjectList') {
-            this.onBoardingSlides[2].options = this.subjectList;
+            this.onBoardingSlides[3].options = this.subjectList;
         } else if (name == 'mediumList') {
-            this.onBoardingSlides[3].options = this.mediumList;
+            this.onBoardingSlides[4].options = this.mediumList;
         }
     }
 
@@ -277,6 +398,7 @@ export class OnboardingService {
             day: -1,
             month: -1,
             standard: -1,
+            syllabus: (_.find(this.onBoardingSlides, ['id', 'syllabusList']).selectedCode.length) ? _.find(this.onBoardingSlides, ['id', 'syllabusList']).selectedCode : this.profile.syllabus,
             board: (_.find(this.onBoardingSlides, ['id', 'boardList']).selectedCode.length) ? _.find(this.onBoardingSlides, ['id', 'boardList']).selectedCode : this.profile.board,
             grade: (_.find(this.onBoardingSlides, ['id', 'gradeList']).selectedCode.length) ? _.find(this.onBoardingSlides, ['id', 'gradeList']).selectedCode : this.profile.grade,
             subject: (_.find(this.onBoardingSlides, ['id', 'subjectList']).selectedCode.length) ? _.find(this.onBoardingSlides, ['id', 'subjectList']).selectedCode : this.profile.subject,
