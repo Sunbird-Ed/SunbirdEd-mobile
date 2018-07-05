e69fcc82210c52ae62ab8c6e9da901b1a4f24f34
Vivek kasture <vivek_kasture@techjoomla.com>
2018-07-05 18:54:29 +0530

    Issue #SB-4484 feat: User and Group Landing page

--- src/pages/user-and-groups/grouplanding/grouplanding.ts
+++ src/pages/user-and-groups/grouplanding/grouplanding.ts
@@ -1,6 +1,6 @@
 import { TranslateService } from '@ngx-translate/core';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ActionSheetController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, PopoverController } from 'ionic-angular';
 
 import { CreateGroupPage } from './../create-group/create-group';
 import { UsersPage } from './../users/users';
@@ -13,35 +13,52 @@ import { CreateuserPage } from '../createuser/createuser';
   templateUrl: 'grouplanding.html',
 })
 export class GrouplandingPage {
-  segmentType: string = "Groups";
-  value: Array<any> = [];
+  segmentType: string = "users";
   groupName: string;
-  isShow: boolean = false;
+  showEmptyUsersMessage: boolean = false;
+  showEmptyGroupsMessage: boolean = false;
+  usersList: Array<any> = [
+    {
+      name: 'Harish BookWala',
+      userType: 'student',
+      grade: 'Grade 2'
+    },
+    {
+      name: 'Nilesh More',
+      userType: 'student',
+      grade: 'Kindergarten'
+    },
+    {
+      name: 'Guru Singh',
+      userType: 'student',
+      grade: 'Grade 1'
+    },
 
-  profession: any;
-  fullname: any;
+  ]
+  groupList: Array<string> = [];
+  userType: string;
   showStyle: false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public translate: TranslateService, public actionSheetCtrl: ActionSheetController) {
-    this.segmentType = "Groups";
-    this.value = this.navParams.get('item');
-    if (this.value) {
-      this.isShow = true;
-      this.groupName = this.value[0]
-    }
-    else {
-      this.isShow = false;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public translate: TranslateService,
+    public actionSheetCtrl: ActionSheetController,
+    public popOverCtrl: PopoverController
+  ) {
+
+    /* Check usersList length and show message or list accordingly */
+    if (this.usersList && this.usersList.length) {
+      this.showEmptyUsersMessage = false;
+    } else {
+      this.showEmptyUsersMessage = true;
     }
 
-    if (this.value) {
-      this.isShow = false;
-      this.fullname = this.value[0].fullname;
-      if (this.value[0].student == true) {
-        this.profession = "student";
-      }
-      else {
-        this.profession = "teacher";
-      }
+    /* Check usersList length and show message or list accordingly */
+    if (this.groupList && this.groupList.length) {
+      this.showEmptyGroupsMessage = false;
+    } else {
+      this.showEmptyGroupsMessage = true;
     }
   }
 
@@ -49,26 +66,34 @@ export class GrouplandingPage {
     console.log('ionViewDidLoad GrouplandingPage');
   }
 
+  /**
+   * Navigates to Create group Page
+   */
   createGroup() {
     this.navCtrl.push(CreateGroupPage, {
     });
   }
 
-  members() {
+  /**
+   * Navigates to group Members page
+   */
+  gotToGroupMembersPage() {
     this.navCtrl.push(GroupMemberPage, {
-      item: this.value
+      item: this.groupList
     })
   }
 
-  users() {
-    this.navCtrl.push(UsersPage, {})
-  }
-
+  /**
+   * Navigates to Create User Page
+   */
   createUser() {
     this.navCtrl.push(CreateuserPage, {})
   }
 
-  presentActionSheet() {
+  /**
+   * Shows Prompt for switch Account
+   */
+  switchAccountPrompt() {
     let actionSheet = this.actionSheetCtrl.create({
       title: 'Are you sure you want to switch accounts you will be signed out from your currently logged account ',
       buttons: [
@@ -89,6 +114,10 @@ export class GrouplandingPage {
     });
     actionSheet.present();
   }
+
+  /**
+   * Returns Style for the border of the box
+   */
   getStyle() {
     if (this.showStyle) {
       return "1px solid #488aff";
@@ -96,4 +125,15 @@ export class GrouplandingPage {
       return "";
     }
   }
+
+  /**
+   * Shows Popover for the edit and delete.
+   * @param myEvent
+   */
+  /* presentPopover(myEvent) {
+    let popover = this.popOverCtrl.create(PopoverPage);
+    popover.present({
+      ev: myEvent
+    });
+  } */
 }
