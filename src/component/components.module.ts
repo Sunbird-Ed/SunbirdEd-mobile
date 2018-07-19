ddec99f0f368dc02a36e8f2af604c4ccd0d79760
Akash Gupta <akash.gupta@tarento.com>
2018-07-20 00:23:08 +0530

    Issue #SB-4505 fix: Added UI for assessment

--- src/component/components.module.ts
+++ src/component/components.module.ts
@@ -7,7 +7,7 @@ import { CourseCard } from "./card/course/course-card";
 import { IncompleteProfileCard } from "./card/incomplete-profile/incomplete-profile-card"
 
 import { HomeAnnouncementCard } from './card/home/home-announcement-card'
-import { IonicPageModule } from 'ionic-angular';
+import { IonicPageModule, IonicModule } from 'ionic-angular';
 import { Ionic2RatingModule } from "ionic2-rating";
 import { IonicImageLoader } from "ionic-image-loader";
 import { ViewMoreActivityListComponent } from './view-more-activity-list/view-more-activity-list';
@@ -45,7 +45,8 @@ import { NgxDatatableModule } from "@swimlane/ngx-datatable";
         Ionic2RatingModule,
         IonicImageLoader,
         PipesModule,
-        NgxDatatableModule
+        NgxDatatableModule,
+        IonicModule
     ],
     exports: [
         CourseCard,
--- src/pages/reports/reports.ts
+++ src/pages/reports/reports.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 //import { ReportListPage } from '../reports/report-list/report-list'
 import { NavController } from 'ionic-angular';
 import { GroupListPage } from './group-list/group-list';
+import {ReportService, ProfileService, GroupService, ProfileRequest} from 'sunbird';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'reports-page',
@@ -9,12 +11,42 @@ import { GroupListPage } from './group-list/group-list';
 })
 export class ReportsPage {
   report: string = 'users';
-  constructor(private navCtrl:NavController) {
+  users;
+  currentUser: {};
+  groups;
+  currentGroups: {};
+  constructor(private navCtrl:NavController, public reportService: ReportService, public profileService: ProfileService, public groupService: GroupService) {}
 
+  ionViewWillEnter() {
+    let that = this;
+    let profileRequest: ProfileRequest = {
+      local: true
+    };
+    this.profileService.getAllUserProfile(profileRequest).then((users) => {
+      users = JSON.parse(users)
+      that.profileService.getCurrentUser(currentUser => {
+        currentUser = JSON.parse(currentUser);
+        users = _.filter(users, function(user) {
+          return user.uid != currentUser.uid
+        })
+        that.users = users;
+        that.currentUser = currentUser;
+      }, error => {
+        console.error("Error", error)
+        return null;
+      })
+    }).catch((error) => {
+      console.log("Something went wrong while fetching user list", error);
+      return null;
+    })
+    this.groupService.getAllGroup().then((groups) => {
+      if (groups.result && groups.result.length) {
+        that.groups = groups;
+      }
+    })
   }
 
-
-  goToReportList(){
+  goToReportList(user){
     this.navCtrl.push(GroupListPage, {
       isFromUsers: true
     });
