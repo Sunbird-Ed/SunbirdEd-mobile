3b1be6ba11c6b2d2036b20dd59ddf44627a57324
Guru123 <gurudayal2k9@gmail.com>
2018-08-13 14:49:09 +0530

    Issue #SB-6310 feat: test cases for user and group module

---
+++ src/pages/user-and-groups/popover/popover.spec.ts
@@ -0,0 +1,72 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { NavParams } from "ionic-angular";
+import { TranslateService, TranslateModule } from "@ngx-translate/core";
+import { PopoverPage } from "./popover";
+
+describe("PopoverPage", () => {
+    let comp: PopoverPage;
+    let fixture: ComponentFixture<PopoverPage>;
+
+    beforeEach(() => {
+       
+        const navControllerStub = {};
+        const navParamsStub = {
+            get: function() {
+                return (function() {})
+            }
+        };
+
+        const translateServiceStub = {
+            get: () => ({
+                subscribe: () => ({})
+            })
+        };
+        TestBed.configureTestingModule({
+            declarations: [ PopoverPage ],
+            schemas: [ NO_ERRORS_SCHEMA ],
+            imports: [TranslateModule.forRoot()],
+            providers: [
+                { provide: NavController, useValue: navControllerStub },
+                { provide: NavParams, useValue: navParamsStub },
+                { provide: TranslateService, useValue: translateServiceStub },
+
+            ]
+        });
+        fixture = TestBed.createComponent(PopoverPage);
+        comp = fixture.componentInstance;
+    });
+
+    it("can load instance", () => {
+        expect(comp).toBeTruthy();
+    });
+
+    it("isCurrentUser defaults to: false", () => {
+        expect(comp.isCurrentUser).toEqual(true);
+    });
+    it('It should fetch delete param', function () {
+        const navParamsStub: NavParams = fixture.debugElement.injector.get(NavParams);
+        spyOn(comp, 'delete').and.callThrough();
+        spyOn(navParamsStub, 'get').and.callThrough();
+        comp.delete();
+        expect(comp.delete).toHaveBeenCalled();
+        expect(navParamsStub.get).toHaveBeenCalled();
+    });
+    it('It should fetch the edit param', function () {
+        const navParamsStub: NavParams = fixture.debugElement.injector.get(NavParams);
+        spyOn(comp, 'edit').and.callThrough();
+        spyOn(navParamsStub, 'get').and.callThrough();
+        comp.edit();
+        expect(comp.edit).toHaveBeenCalled();
+        expect(navParamsStub.get).toHaveBeenCalled();
+    });
+    it('It should call the ionViewDidLoad method' , function () {
+
+        spyOn(comp, 'ionViewDidLoad').and.callThrough();
+        comp.ionViewDidLoad();
+        expect(comp.ionViewDidLoad).toHaveBeenCalled();
+
+    });
+
+});
