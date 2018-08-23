65a15c224c86fdfeac2e54b80c672670bce6898c
subranil <subranil.saha@tarento.com>
2018-08-23 20:22:37 +0530

    Issue #SB-6310 feat: Added test-case  about-app,about-us, privacypolicy

--- src/pages/settings/privacypolicy/privacypolicy.ts
+++ src/pages/settings/privacypolicy/privacypolicy.ts
@@ -17,20 +17,16 @@ export class PrivacypolicyPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PrivacypolicyPage');
-  }
-
-  ionViewDidEnter() {
+  /*ionViewDidEnter() {
     this.generateImpressionEvent();
   }
 
   generateImpressionEvent() {
-    // let impression = new Impression();
-    // impression.type = ImpressionType.VIEW;
-    // impression.pageId = PageId.SETTINGS_ABOUT_US;
-    // impression.env = Environment.SETTINGS;
-    // this.telemetryService.impression(impression);
-  }
+    let impression = new Impression();
+    impression.type = ImpressionType.VIEW;
+    impression.pageId = PageId.SETTINGS_ABOUT_US;
+    impression.env = Environment.SETTINGS;
+    this.telemetryService.impression(impression);
+  }*/
 
 }
