7c4d58defcaf9ed95ede5181b5e9893889854f1f
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2018-08-31 16:54:06 +0530

    Issue #SB-6310 feat: unit test fixes for education form

--- test-config/mocks-ionic.ts
+++ test-config/mocks-ionic.ts
@@ -445,6 +445,20 @@ export class BuildParamaServiceMock {
   }
 }
 
+export class IonicAppMock {
+
+  _modalPortal: {
+    getActive: () => ({
+        dismiss: () =>{}
+    })
+  }
+  _overlayPortal: {
+    getActive: () => ({
+        dismiss: () =>{}
+    })
+  }
+}
+
 
 /*export class ToastMock {
   public static instance(): any {
