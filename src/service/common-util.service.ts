4e6e278af6742a006dd285127223b2dccdb95275
Vivek Kasture <vivek_kasture@techjoomla.com>
2018-09-05 16:09:07 +0530

    Issue #SB-5522 feat: Added translation for search filters

--- src/service/common-util.service.ts
+++ src/service/common-util.service.ts
@@ -28,10 +28,10 @@ export class CommonUtilService {
     }
 
     /**
-   * Used to Translate message to current Language
-   * @param {string} messageConst - Message Constant to be translated
-   * @returns {string} translatedMsg - Translated Message
-   */
+     * Used to Translate message to current Language
+     * @param {string} messageConst - Message Constant to be translated
+     * @returns {string} translatedMsg - Translated Message
+     */
     translateMessage(messageConst: string, field?: string): string {
         let translatedMsg = '';
         this.translate.get(messageConst, { '%s': field }).subscribe(
@@ -40,5 +40,19 @@ export class CommonUtilService {
             }
         );
         return translatedMsg;
+    }
+
+    /**
+     * 
+     * @param {string} translations Stringified object of translations 
+     * @param {string} defaultValue Fallback value if does not have translations
+     * @returns {string} Translated values or fallback value
+     */
+    getTranslatedValue(translations: string, defaultValue: string) {
+        let availableTranslation = JSON.parse(translations);
+        if (availableTranslation.hasOwnProperty(this.translate.currentLang)) {
+            return availableTranslation[this.translate.currentLang];
+        }
+        return defaultValue;
     }
 }
