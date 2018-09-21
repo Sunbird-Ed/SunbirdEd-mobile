c6940828d206d81f5e8a6401a25633963c8bf404
Guru123 <gurudayal2k9@gmail.com>
2018-09-21 14:14:51 +0530

    Issue #SB-6310 feat: test cases of  group-details page

--- src/pages/user-and-groups/group-details/group-detail.data.spec.ts
+++ src/pages/user-and-groups/group-details/group-detail.data.spec.ts
@@ -1,152 +1,165 @@
 export const mockAllProfiles = [
-    {
-      "age": -1,
-      "avatar": "avatar",
-      "createdAt": "Aug 9, 2018 11:36:50 AM",
-      "day": -1,
-      "gender": "",
-      "handle": "vivek",
-      "isGroupUser": false,
-      "language": "en",
-      "month": -1,
-      "profileType": "STUDENT",
-      "source": "LOCAL",
-      "standard": -1,
-      "uid": "3af2e8a4-003e-438d-b360-2ae922696913"
-    },
-    {
-      "age": -1,
-      "avatar": "avatar",
-      "createdAt": "Jul 27, 2018 12:37:28 PM",
-      "day": -1,
-      "gender": "",
-      "handle": "Guru",
-      "isGroupUser": false,
-      "language": "en",
-      "month": -1,
-      "profileType": "TEACHER",
-      "source": "LOCAL",
-      "standard": -1,
-      "uid": "97428028-641c-4e57-aef3-74b52ce850f7"
-    },
-    {
-      "age": -1,
-      "avatar": "avatar",
-      "createdAt": "Aug 2, 2018 12:09:14 PM",
-      "day": -1,
-      "gender": "",
-      "handle": "Diptesh",
-      "isGroupUser": false,
-      "language": "en",
-      "month": -1,
-      "profileType": "STUDENT",
-      "source": "LOCAL",
-      "standard": -1,
-      "syllabus": [
-        "mh_k-12_15"
-      ],
-      "uid": "47513f4f-fbb3-40c4-ab3d-8b9059277894"
-    }
-  ];
+  {
+    "age": -1,
+    "avatar": "avatar",
+    "createdAt": "Aug 9, 2018 11:36:50 AM",
+    "day": -1,
+    "gender": "",
+    "handle": "vivek",
+    "isGroupUser": false,
+    "language": "en",
+    "month": -1,
+    "profileType": "STUDENT",
+    "source": "LOCAL",
+    "standard": -1,
+    "uid": "3af2e8a4-003e-438d-b360-2ae922696913",
+    grade: "1qwqqw"
 
-  export const userList=
-  [
-    {
-      "age": -1,
+  },
+  {
+    "age": -1,
+    "avatar": "avatar",
+    "createdAt": "Jul 27, 2018 12:37:28 PM",
+    "day": -1,
+    "gender": "",
+    "handle": "Guru",
+    "isGroupUser": false,
+    "language": "en",
+    "month": -1,
+    "profileType": "TEACHER",
+    "source": "LOCAL",
+    "standard": -1,
+    "uid": "97428028-641c-4e57-aef3-74b52ce850f7"
+  },
+  {
+    "age": -1,
+    "avatar": "avatar",
+    "createdAt": "Aug 2, 2018 12:09:14 PM",
+    "day": -1,
+    "gender": "",
+    "handle": "Diptesh",
+    "isGroupUser": false,
+    "language": "en",
+    "month": -1,
+    "profileType": "STUDENT",
+    "source": "LOCAL",
+    "standard": -1,
+    "syllabus": [
+      "mh_k-12_15"
+    ],
+    "uid": "47513f4f-fbb3-40c4-ab3d-8b9059277894"
+  },
+
+];
+
+export const userList=
+[
+  {
+    "age": -1,
       "avatar": "avatar",
       "createdAt": "Aug 9, 2018 11:36:50 AM",
       "day": -1,
       "gender": "",
-      "handle": "Abdbdjdjdjdjdjdjsjdjdjdjdjdjjdjdjdjrjejrjrjrjejejejejejejejggggghhhhhhhhh",
+      "handle": "handle_testdata",
       "isGroupUser": false,
       "language": "en",
       "month": -1,
-      "profileType": "STUDENT",
+     "profileType": "STUDENT",
       "source": "LOCAL",
       "standard": -1,
-      "uid": "3af2e8a4-003e-438d-b360-2ae922696913"
-    },
-    {
-      "age": -1,
-      "avatar": "avatar",
-      "createdAt": "Jul 27, 2018 12:37:28 PM",
-      "day": -1,
-      "gender": "",
-      "handle": "Guest1",
-      "isGroupUser": false,
-      "language": "en",
-      "month": -1,
-      "profileType": "TEACHER",
-      "source": "LOCAL",
-      "standard": -1,
-      "uid": "97428028-641c-4e57-aef3-74b52ce850f7"
-    },
-    {
-      "age": -1,
-      "avatar": "avatar",
-      "createdAt": "Aug 2, 2018 12:09:14 PM",
-      "day": -1,
-      "gender": "",
-      "handle": "Gurhxbzjjzjzzjjzjzjdxhdhhdjdjdjfjfdjdhdjsjjsjskdkdkdkdkskdkdkdkd",
-      "isGroupUser": false,
-      "language": "en",
-      "month": -1,
-      "profileType": "STUDENT",
-      "source": "LOCAL",
-      "standard": -1,
-      "syllabus": [
-        "mh_k-12_15"
-      ],
-      "uid": "47513f4f-fbb3-40c4-ab3d-8b9059277894"
-    }
-  ]
-  export const selectedUser =[
-    {
-      "age": -1,
-      "avatar": "avatar",
-      "createdAt": "Aug 9, 2018 11:36:50 AM",
-      "day": -1,
-      "gender": "",
-      "handle": "Abdbdjdjdjdjdjdjsjdjdjdjdjdjjdjdjdjrjejrjrjrjejejejejejejejggggghhhhhhhhh",
-      "isGroupUser": false,
-      "language": "en",
-      "month": -1,
-      "profileType": "STUDENT",
-      "source": "LOCAL",
-      "standard": -1,
-      "uid": "3af2e8a4-003e-438d-b360-2ae922696913"
-    },
-    {
-      "age": -1,
-      "avatar": "avatar",
-      "createdAt": "Jul 27, 2018 12:37:28 PM",
-      "day": -1,
-      "gender": "",
-      "handle": "Guest1",
-      "isGroupUser": false,
-      "language": "en",
-      "month": -1,
-      "profileType": "TEACHER",
-      "source": "LOCAL",
-      "standard": -1,
-      "uid": "97428028-641c-4e57-aef3-74b52ce850f7"
-    },
-    {
-      "age": -1,
-      "avatar": "avatar",
-      "createdAt": "Aug 2, 2018 12:09:14 PM",
-      "day": -1,
-      "gender": "",
-      "handle": "Gurhxbzjjzjzzjjzjzjdxhdhhdjdjdjfjfdjdhdjsjjsjskdkdkdkdkskdkdkdkd",
-      "isGroupUser": false,
-      "language": "en",
-      "month": -1,
-      "profileType": "STUDENT",
-      "source": "LOCAL",
-      "standard": -1,
-      "syllabus": [
-        "mh_k-12_15"
-      ],
-      "uid": "47513f4f-fbb3-40c4-ab3d-8b9059277894"
-    }
-  ]
+      "uid": "sample_id1",
+    //  "profileType": ProfileType.STUDENT,
+      "syllabus":[]
+
+  },
+  {
+    "age": -1,
+    "avatar": "avatar",
+    "createdAt": "Jul 27, 2018 12:37:28 PM",
+    "day": -1,
+    "gender": "",
+    "handle": "Guest1",
+    "isGroupUser": false,
+    "language": "en",
+    "month": -1,
+    "profileType": "TEACHER",
+    "source": "LOCAL",
+    "standard": -1,
+    "uid": "sample_id2"
+  },
+  {
+    "age": -1,
+    "avatar": "avatar",
+    "createdAt": "Aug 2, 2018 12:09:14 PM",
+    "day": -1,
+    "gender": "",
+    "handle": "Gurhxbzjjzjzzjjzjzjdxhdhhdjdjdjfjfdjdhdjsjjsjskdkdkdkdkskdkdkdkd",
+    "isGroupUser": false,
+    "language": "en",
+    "month": -1,
+    "profileType": "STUDENT",
+    "source": "LOCAL",
+    "standard": -1,
+    "syllabus": [
+      "mh_k-12_15"
+    ],
+    "uid": "47513f4f-fbb3-40c4-ab3d-8b9059277894"
+  }
+]
+export const selectedUser =[
+  {
+    "age": -1,
+    "avatar": "avatar",
+    "createdAt": "Aug 9, 2018 11:36:50 AM",
+    "day": -1,
+    "gender": "",
+    "handle": "Abdbdjdjdjdjdjdjsjdjdjdjdjdjjdjdjdjrjejrjrjrjejejejejejejejggggghhhhhhhhh",
+    "isGroupUser": false,
+    "language": "en",
+    "month": -1,
+    "profileType": "STUDENT",
+    "source": "LOCAL",
+    "standard": -1,
+    "uid": "3af2e8a4-003e-438d-b360-2ae922696913"
+  },
+  {
+    "age": -1,
+    "avatar": "avatar",
+    "createdAt": "Jul 27, 2018 12:37:28 PM",
+    "day": -1,
+    "gender": "",
+    "handle": "Guest1",
+    "isGroupUser": false,
+    "language": "en",
+    "month": -1,
+    "profileType": "TEACHER",
+    "source": "LOCAL",
+    "standard": -1,
+    "uid": "97428028-641c-4e57-aef3-74b52ce850f7"
+  },
+  {
+    "age": -1,
+    "avatar": "avatar",
+    "createdAt": "Aug 2, 2018 12:09:14 PM",
+    "day": -1,
+    "gender": "",
+    "handle": "Gurhxbzjjzjzzjjzjzjdxhdhhdjdjdjfjfdjdhdjsjjsjskdkdkdkdkskdkdkdkd",
+    "isGroupUser": false,
+    "language": "en",
+    "month": -1,
+    "profileType": "STUDENT",
+    "source": "LOCAL",
+    "standard": -1,
+    "syllabus": [
+      "mh_k-12_15"
+    ],
+    "uid": "47513f4f-fbb3-40c4-ab3d-8b9059277894"
+  }
+]
+
+export const userUids  =
+
+   ["sample_id1", "sample_id2", "sample_id3"]
+
+
+
