91eb6501d1dc4e990dd3a919fe371e802dea2a63
Anil Gupta <ani2824@gmail.com>
2018-09-28 19:08:13 +0530

    Issue #SB-7042 chore: Fixed lint errors.

--- src/pages/search/filters/options/option.spec.ts
+++ src/pages/search/filters/options/option.spec.ts
@@ -1,25 +1,30 @@
-import { async, TestBed, ComponentFixture } from '@angular/core/testing';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { HttpClientModule } from "@angular/common/http";
+import {
+    async,
+    TestBed,
+    ComponentFixture
+} from '@angular/core/testing';
+import {
+    TranslateModule,
+    TranslateLoader
+} from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
 import { PipesModule } from './../../../../pipes/pipes.module';
 import {
-    IonicModule, NavParams,ViewController,Platform
+    IonicModule, NavParams, ViewController, Platform
 } from 'ionic-angular';
-
-
 import {
-    FrameworkModule} from "sunbird";
-
+    FrameworkModule
+} from 'sunbird';
 import {
-    TranslateLoaderMock, NavParamsMockNew} from '../../../../../test-config/mocks-ionic';
-
+    TranslateLoaderMock, NavParamsMockNew
+} from '../../../../../test-config/mocks-ionic';
 import { } from 'jasmine';
 import { FilterOptions } from './../../filters/options/options';
-import { mockView } from "ionic-angular/util/mock-providers";
+import { mockView } from 'ionic-angular/util/mock-providers';
 describe('SearchPage Component', () => {
     let component: FilterOptions;
     let fixture: ComponentFixture<FilterOptions>;
-    let viewControllerMock = mockView();
+    const viewControllerMock = mockView();
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [FilterOptions],
@@ -36,31 +41,22 @@ describe('SearchPage Component', () => {
                 { provide: NavParams, useClass: NavParamsMockNew },
                 { provide: ViewController, useValue: viewControllerMock },
             ]
-        })
+        });
     }));
 
     beforeEach(() => {
         const platform = TestBed.get(Platform);
-        spyOn(platform, 'registerBackButtonAction').and.callFake(function (success) {
+        spyOn(platform, 'registerBackButtonAction').and.callFake((success) => {
             return success(jasmine.anything);
         });
         fixture = TestBed.createComponent(FilterOptions);
         component = fixture.componentInstance;
     });
 
-
-    it("#confirm should dissmiss the View contoller", function () {
+    it('#confirm should dissmiss the View contoller',  () => {
         const viewController = TestBed.get(ViewController);
-        spyOn(viewController,'dismiss');
+        spyOn(viewController, 'dismiss');
         component.confirm();
         expect(viewController.dismiss).toHaveBeenCalled();
-     });
-
+    });
 });
-
-
-
-
-
-
-
--- src/util/filter.util.ts
+++ src/util/filter.util.ts
@@ -1,8 +1,8 @@
-import { AppGlobalService } from "../service/app-global.service";
+import { AppGlobalService } from '../service/app-global.service';
 
 function applyProfileFilter(profileFilter: Array<any>, assembleFilter: Array<any>, categoryKey: string, appGlobal: AppGlobalService) {
     if (categoryKey) {
-        let nameArray = [];
+        const nameArray = [];
         profileFilter.forEach(filterCode => {
             let nameForCode = appGlobal.getNameForCodeInFramework(categoryKey, filterCode);
 
@@ -11,22 +11,21 @@ function applyProfileFilter(profileFilter: Array<any>, assembleFilter: Array<any
             }
 
             nameArray.push(nameForCode);
-        })
+        });
 
         profileFilter = nameArray;
     }
-
 
     if (!assembleFilter) {
         assembleFilter = [];
     }
     assembleFilter = assembleFilter.concat(profileFilter);
 
-    let unique_array = [];
+    const unique_array = [];
 
     for (let i = 0; i < assembleFilter.length; i++) {
-        if (unique_array.indexOf(assembleFilter[i]) == -1 && assembleFilter[i].length > 0) {
-            unique_array.push(assembleFilter[i])
+        if (unique_array.indexOf(assembleFilter[i]) === -1 && assembleFilter[i].length > 0) {
+            unique_array.push(assembleFilter[i]);
         }
     }
 
@@ -34,46 +33,46 @@ function applyProfileFilter(profileFilter: Array<any>, assembleFilter: Array<any
 }
 
 export function updateFilterInSearchQuery(queryParams, appliedFilter, profile, mode, isFilterApplied, appGlobal: AppGlobalService) {
-    let queryObj = JSON.parse(queryParams);
-    let filter = queryObj.request.filters;
+    const queryObj = JSON.parse(queryParams);
+    const filter = queryObj.request.filters;
 
-    if (mode === "soft") {
+    if (mode === 'soft') {
         queryObj.request.mode = mode;
     }
 
     if (appliedFilter) {
-        let appliedFilterKey = Object.keys(appliedFilter);
+        const appliedFilterKey = Object.keys(appliedFilter);
 
         appliedFilterKey.forEach(key => {
             if (appliedFilter[key].length > 0) {
                 if (!filter[key]) {
-                    filter[key] = []
+                    filter[key] = [];
                 }
 
                 appliedFilter[key].forEach(filterValue => {
                     if (!filter[key].includes(filterValue)) {
                         filter[key].push(filterValue);
                     }
-                })
+                });
             }
-        })
+        });
     }
 
     if (profile && !isFilterApplied) {
         if (profile.board && profile.board.length) {
-            filter["board"] = applyProfileFilter(profile.board, filter["board"], "board", appGlobal);
+            filter['board'] = applyProfileFilter(profile.board, filter['board'], 'board', appGlobal);
         }
 
         if (profile.medium && profile.medium.length) {
-            filter["medium"] = applyProfileFilter(profile.medium, filter["medium"], "medium", appGlobal);
+            filter['medium'] = applyProfileFilter(profile.medium, filter['medium'], 'medium', appGlobal);
         }
 
         if (profile.grade && profile.grade.length) {
-            filter["gradeLevel"] = applyProfileFilter(profile.grade, filter["gradeLevel"], "gradeLevel", appGlobal);
+            filter['gradeLevel'] = applyProfileFilter(profile.grade, filter['gradeLevel'], 'gradeLevel', appGlobal);
         }
 
         if (profile.subject && profile.subject.length) {
-            filter["subject"] = applyProfileFilter(profile.subject, filter["subject"], "subject", appGlobal);
+            filter['subject'] = applyProfileFilter(profile.subject, filter['subject'], 'subject', appGlobal);
         }
     }
 
