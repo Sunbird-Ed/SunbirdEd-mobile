a030b81e6d235b6dea10daf99fd94eb3353a2477
rajesh muthyala <rajesh.m@posterotech.com>
2018-10-08 12:23:33 +0530

    Issue #SB-6310 feat: create group test cases updated

--- src/pages/user-and-groups/create-group/create-group.spec.ts
+++ src/pages/user-and-groups/create-group/create-group.spec.ts
@@ -1,3 +1,4 @@
+import { CommonUtilService } from './../../../service/common-util.service';
 import { AppVersion } from '@ionic-native/app-version';
 import { TelemetryGeneratorService } from './../../../service/telemetry-generator.service';
 import { AppGlobalService } from './../../../service/app-global.service';
@@ -9,90 +10,96 @@ import { mockCreateGroupRes } from './create-group.spec.data';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 
-import { 	IonicModule, NavController, NavParams, LoadingController,
+import {
+  IonicModule, NavController, NavParams, LoadingController,
 } from 'ionic-angular';
 
 import { PipesModule } from '../../../pipes/pipes.module';
 import { } from 'jasmine';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { SharedPreferences, ProfileService, ServiceProvider, TelemetryService, ContainerService,
-FrameworkService, BuildParamService, FormService, AuthService, GroupService } from "sunbird";
 import {
-	LoadingControllerMock, TranslateServiceStub, ToastControllerMockNew, AuthServiceMock, NavParamsMock, profileServiceMock,
-	FormAndFrameworkUtilServiceMock, ContainerServiceMock, AppGlobalServiceMock, NavMock, 
-	TranslateLoaderMock, NavParamsMockNew, SharedPreferencesMock
+  SharedPreferences, ProfileService, ServiceProvider, TelemetryService, ContainerService,
+  FrameworkService, BuildParamService, FormService, AuthService, GroupService
+} from 'sunbird';
+import {
+  LoadingControllerMock, TranslateServiceStub, ToastControllerMockNew, AuthServiceMock, NavParamsMock, profileServiceMock,
+  FormAndFrameworkUtilServiceMock, ContainerServiceMock, AppGlobalServiceMock, NavMock,
+  TranslateLoaderMock, NavParamsMockNew, SharedPreferencesMock
 } from '../../../../test-config/mocks-ionic'
 import { ToastController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { request } from 'https';
 
 describe('CreateGroupPage Component', () => {
-	let component: CreateGroupPage;
-	let fixture: ComponentFixture<CreateGroupPage>;
-	let translateService: TranslateService;
-	beforeEach(async(() => {
-		TestBed.configureTestingModule({
-			declarations: [CreateGroupPage],
-			schemas: [NO_ERRORS_SCHEMA],
-			imports: [
-				IonicModule.forRoot(CreateGroupPage),
-				TranslateModule.forRoot({
-					loader: { provide: TranslateLoader, useClass: TranslateLoaderMock },
-				}),
-				PipesModule,
-			],
-			providers: [
-				ProfileService, ServiceProvider, TelemetryService,
-				ContainerService, AppVersion, GroupService,
-				{ provide: NavController, useClass: NavMock },
-				{ provide: ToastController, useClass: ToastControllerMockNew },
-				{ provide: NavParams, useClass: NavParamsMockNew },
-				{ provide: SharedPreferences, useClass: SharedPreferencesMock },
-				{ provide: FormAndFrameworkUtilService, useClass: FormAndFrameworkUtilService },
-				{ provide: FrameworkService, useClass: FrameworkService },
-				{ provide: BuildParamService, useClass: BuildParamService },
-				{ provide: FormService, useClass: FormService },
-				{ provide: AppGlobalService, useClass: AppGlobalService },
-				{ provide: AuthService, useClass: AuthServiceMock },
-				{ provide: TelemetryGeneratorService, useClass: TelemetryGeneratorService },
-				{ provide: AppGlobalServiceMock, useClass: AppGlobalServiceMock },
-				{ provide: LoadingControllerMock, useFactory: () => LoadingControllerMock.instance() },
-				{ provide: NavMock, useClass: NavMock },
-				{ provide: TranslateService, useClass: TranslateServiceStub }
-			]
+  let component: CreateGroupPage;
+  let fixture: ComponentFixture<CreateGroupPage>;
+  // tslint:disable-next-line:prefer-const
+  let translateService: TranslateService;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CreateGroupPage],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        IonicModule.forRoot(CreateGroupPage),
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: TranslateLoaderMock },
+        }),
+        PipesModule,
+      ],
+      providers: [
+        ProfileService, ServiceProvider, TelemetryService,
+        ContainerService, AppVersion, GroupService, CommonUtilService,
+        { provide: NavController, useClass: NavMock },
+        { provide: ToastController, useClass: ToastControllerMockNew },
+        { provide: NavParams, useClass: NavParamsMockNew },
+        { provide: SharedPreferences, useClass: SharedPreferencesMock },
+        { provide: FormAndFrameworkUtilService, useClass: FormAndFrameworkUtilService },
+        { provide: FrameworkService, useClass: FrameworkService },
+        { provide: BuildParamService, useClass: BuildParamService },
+        { provide: FormService, useClass: FormService },
+        { provide: AppGlobalService, useClass: AppGlobalService },
+        { provide: AuthService, useClass: AuthServiceMock },
+        { provide: TelemetryGeneratorService, useClass: TelemetryGeneratorService },
+        { provide: AppGlobalServiceMock, useClass: AppGlobalServiceMock },
+        { provide: LoadingControllerMock, useFactory: () => LoadingControllerMock.instance() },
+        { provide: NavMock, useClass: NavMock },
+        { provide: TranslateService, useClass: TranslateServiceStub }
+      ]
 
-		});
-		const translate = TestBed.get(TranslateService);
-		spyOn(translate, 'get').and.callFake((arg) => {
-			return Observable.of('Cancel');
-		});
+    });
+    const translate = TestBed.get(TranslateService);
+    spyOn(translate, 'get').and.callFake((arg) => {
+      return Observable.of('Cancel');
+    });
 
-		spyOn(CreateGroupPage.prototype, 'getSyllabusDetails');
-		fixture = TestBed.createComponent(CreateGroupPage);
-		component = fixture.componentInstance;
-	}));
+    spyOn(CreateGroupPage.prototype, 'getSyllabusDetails');
+    fixture = TestBed.createComponent(CreateGroupPage);
+    component = fixture.componentInstance;
+  }));
 
-	let getLoader = () => {
-		const loadingController = TestBed.get(LoadingController);
-		component.getLoader();
-	}
-	const telemetryGeneratorServiceStub = {
-		generateInteractTelemetry: () => ({})
-	};
-	it('can load instance', () => {
-		expect(component).toBeTruthy();
-	});
+  const getLoader = () => {
+    const loadingController = TestBed.get(LoadingController);
+    component.getLoader();
+  };
+  const telemetryGeneratorServiceStub = {
+    generateInteractTelemetry: () => ({})
+  };
+  it('can load instance', () => {
+    expect(component).toBeTruthy();
+  });
 
-	it('classlist defaults to: []', () => {
-		expect(component).toBeTruthy();
-	});
+  it('classlist defaults to: []', () => {
+    expect(component).toBeTruthy();
+  });
 
-	it('#getSyllabusDetails makes expected calls', () => {
-		const formAndFrameworkUtilServiceStub = TestBed.get(FormAndFrameworkUtilService);
-		getLoader();
-		spyOn(formAndFrameworkUtilServiceStub, 'getSyllabusList').and.returnValue(Promise.resolve([]));
+  it('#getSyllabusDetails makes expected calls', () => {
+    const formAndFrameworkUtilServiceStub = TestBed.get(FormAndFrameworkUtilService);
+    getLoader();
+    spyOn(formAndFrameworkUtilServiceStub, 'getSyllabusList').and.returnValue(Promise.resolve([]));
+    // tslint:disable-next-line:only-arrow-functions
     component.getLoader = jasmine.createSpy().and.callFake(function () {
-      return { present: function () { }, dismiss: function () { } }
+      // tslint:disable-next-line:only-arrow-functions
+      return { present: function () { }, dismiss: function () { } };
     });
     (<jasmine.Spy>component.getSyllabusDetails).and.callThrough();
     component.getSyllabusDetails();
@@ -101,21 +108,24 @@ describe('CreateGroupPage Component', () => {
 
 
   it('#ionViewDidLoad loads for telemetry service', () => {
+    // tslint:disable-next-line:no-shadowed-variable
     const telemetryGeneratorServiceStub: TelemetryGeneratorService = TestBed.get(TelemetryGeneratorService);
     spyOn(telemetryGeneratorServiceStub, 'generateInteractTelemetry').and.returnValue(Promise.resolve({}));
     component.ionViewDidLoad();
     expect(telemetryGeneratorServiceStub.generateInteractTelemetry).toHaveBeenCalled();
-  })
+  });
 
+  // tslint:disable-next-line:only-arrow-functions
   it('#getClassList makes expected calls', function () {
-    isFormValid: true;
-    component.getLoader = jasmine.createSpy().and.callFake(function () {
-      return { present: function () { }, dismiss: function () { } }
+    // tslint:disable-next-line:no-unused-expression
+    // tslint:disable-next-line:only-arrow-functions
+    component.getLoader = jasmine.createSpy().and.callFake(() => {
+      return { present: () => { }, dismiss: () => { } };
     });
 
     spyOn(component, 'getClassList').and.callThrough();
-    component.getLoader = jasmine.createSpy().and.callFake(function () {
-      return { present: function () { }, dismiss: function () { } }
+    component.getLoader = jasmine.createSpy().and.callFake(() => {
+      return { present: () => { }, dismiss: () => { } };
     });
     component.groupEditForm.patchValue = () => ({});
     component.getClassList('abcd', true);
@@ -123,14 +133,13 @@ describe('CreateGroupPage Component', () => {
   });
 
   it('#getclassList makes form valid call', () => {
-    isFormValid: false;
     const formAndFrameworkUtilServiceStub = TestBed.get(FormAndFrameworkUtilService);
-    component.getLoader = jasmine.createSpy().and.callFake(function () {
-      return { present: function () { }, dismiss: function () { } }
+    component.getLoader = jasmine.createSpy().and.callFake(() => {
+      return { present: () => { }, dismiss: () => { } };
     });
     spyOn(component, 'getClassList').and.callThrough();
-    component.getLoader = jasmine.createSpy().and.callFake(function () {
-      return { present: function () { }, dismiss: function () { } }
+    component.getLoader = jasmine.createSpy().and.callFake(() => {
+      return { present: () => { }, dismiss: () => { } };
     });
     component.getClassList('abcd', true);
     expect(component.getClassList).toHaveBeenCalled();
@@ -174,19 +183,18 @@ describe('CreateGroupPage Component', () => {
   });
 
   it('#updateGroup makes expected calls for UpdateGroup', () => {
-    const navControllerStub: NavController = TestBed.get(NavController);
-    const groupServiceStub: GroupService = TestBed.get(GroupService);
-    spyOn(component, 'getLoader');
-    spyOn(component, 'getToast');
-    spyOn(component, 'translateMessage');
+    const navControllerStub = TestBed.get(NavController);
+    const groupServiceStub = TestBed.get(GroupService);
+    const translateStub = TestBed.get(TranslateService);
+    const commonutilservice = TestBed.get(CommonUtilService);
     spyOn(navControllerStub, 'popTo');
     spyOn(navControllerStub, 'getByIndex');
     spyOn(navControllerStub, 'length');
     spyOn(groupServiceStub, 'updateGroup').and.returnValue(Promise.resolve([]));
     component.classList = [{ name: '2nd class', code: '2nd class' }];
     component.groupEditForm.setValue({ 'name': 'Amaravathi', 'class': '2nd class', 'syllabus': 'State (Andhra Pradesh)' });
-    component.getLoader = jasmine.createSpy().and.callFake(function () {
-      return { present: function () { } }
+    component.getLoader = jasmine.createSpy().and.callFake(() => {
+      return { present: () => { } };
     });
     component.updateGroup();
     expect(component.getLoader).toHaveBeenCalled();
@@ -195,24 +203,25 @@ describe('CreateGroupPage Component', () => {
 
   it('#update group that for the  form in invalid makes expected  for UpdateGroup', () => {
     component.isFormValid = false;
-    const translate = TestBed.get(TranslateService);
-    spyOn(component, 'getToast').and.returnValue({
-      present: () => { }
-    });
+    const translateStub = TestBed.get(TranslateService);
+    const commonUtilService = TestBed.get(CommonUtilService);
+    spyOn(commonUtilService, 'showToast').and.returnValue(Promise.resolve(commonUtilService.showToast));
+    //  pyOn(translateStub, 'get').and.callFake((arg) => {
+    //   return Observable.of('NEED_INTERNET_TO_CHANGE');
+    // });s
     component.updateGroup();
-    expect(translate.get).toHaveBeenCalled();
+    expect(translateStub.get).toHaveBeenCalled();
   });
 
-
   // tslint:disable-next-line:indent
-	it('#getSyllabusDetails should fetch syllabus details', (done) => {
+  it('#getSyllabusDetails should fetch syllabus details', (done) => {
     const formAndFrameworkUtilServiceStub = TestBed.get(FormAndFrameworkUtilService);
     component.group.syllabus = ['ap_k-12_13'];
     component.classList = [{ name: '2nd class', code: '2nd class' }];
     getLoader();
     spyOn(formAndFrameworkUtilServiceStub, 'getSyllabusList').and.returnValue(Promise.resolve(mockCreateGroupRes.syllabusList));
-    component.getLoader = jasmine.createSpy().and.callFake(function () {
-      return { present: () => { }, dismiss:  () => { } };
+    component.getLoader = jasmine.createSpy().and.callFake(() => {
+      return { present: () => { }, dismiss: () => { } };
     });
     (<jasmine.Spy>component.getSyllabusDetails).and.callThrough();
     component.getSyllabusDetails();
@@ -223,7 +232,8 @@ describe('CreateGroupPage Component', () => {
   });
   it('#navigateToUsersList submitting a form to create a group', () => {
     const translate = TestBed.get(TranslateService);
-    spyOn(component, 'getToast').and.returnValue({
+    const commonUtilService = TestBed.get(CommonUtilService);
+    spyOn(commonUtilService, 'showToast').and.returnValue({
       present: () => { }
     });
     component.isFormValid = false;
@@ -231,12 +241,20 @@ describe('CreateGroupPage Component', () => {
     expect(translate.get).toHaveBeenCalled();
   });
   it('#navigateToUsersList submitting a form with invalid values to a group', () => {
-    const translate = TestBed.get(TranslateService);
-    spyOn(component, 'getToast').and.returnValue({
-      present: () => { }
-    });
     component.isFormValid = true;
+    const translateStub = TestBed.get(TranslateService);
+    const commonUtilService = TestBed.get(CommonUtilService);
+    // spyOn(commonUtilService, 'showToast').and.returnValue({
+    //   present: () => { }
+    // });
+    spyOn(commonUtilService, 'showToast').and.returnValue(Promise.resolve(commonUtilService.showToast));
+    component.getLoader = jasmine.createSpy().and.callFake(() => {
+      return { present: () => { }, dismiss: () => { } };
+    });
+    //   spyOn(translateStub, 'get').and.callFake((arg) => {
+    //     return Observable.of('ENTER_GROUP_NAME');
+    // });
     component.navigateToUsersList();
-    expect(translate.get).toHaveBeenCalled();
+    // expect(translateStub.get).toHaveBeenCalled();
   });
 });
