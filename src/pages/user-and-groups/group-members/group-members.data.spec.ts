62316fae40b1aa3ee15342423318c18b2255f517
Ajoy Maity <ajoy.maity@tarento.com>
2018-10-12 16:36:13 +0530

    Issue #SB-6310 feat: Fixed TestCase Error for Group Members

--- src/pages/user-and-groups/group-members/group-members.data.spec.ts
+++ src/pages/user-and-groups/group-members/group-members.data.spec.ts
@@ -1,51 +1,56 @@
+import { ProfileType, UserSource } from 'sunbird';
 export const mockRes = {
-    facets: {
-      name: 'board',
-      displayName: 'BOARD',
-      values: [
-        'sample_1',
-        'sam2',
-        'sam3',
-        'sam4',
-        'sam5',
-        'sam6'
-      ],
-      selected: ['sam4'],
-    },
-    facetsWithoutSelectedValue: {
-      name: 'board',
-      displayName: 'BOARD',
-      values: [
-          'sam1',
-          'sam2',
-          'sam3',
-          'sam4',
-          'sam5',
-          'sam6'
-      ]
-    }
-   };
+  facets: {
+    name: 'board',
+    displayName: 'BOARD',
+    values: [
+      'sample_1',
+      'sample_2',
+      'sample_3',
+      'sample_4',
+      'sample_5',
+      'sample_6'
+    ],
+    selected: ['sample_4'],
+  },
+  facetsWithoutSelectedValue: {
+    name: 'board',
+    displayName: 'BOARD',
+    values: [
+      'sample_1',
+      'sample_2',
+      'sample_3',
+      'sample_4',
+      'sample_5',
+      'sample_6'
+    ]
+  },
+  profile :
+    { handle: 'sample', syllabus: ['NCF'], board: ['CBSE'], grade: ['KG'], subject: ['English'], medium: ['English'],
+    profileType: ProfileType.TEACHER, source: UserSource.LOCAL }
+
+};
 //    describe('isSelected', () => {
 //       it('should be select the value', () => {
 //         comp.facets = mockRes.facets;
-//         comp.isSelected('sam2');
+//         comp.isSelected('sample_2');
 
 //         comp.facets = mockRes.facetsWithoutSelectedValue;
-//         comp.isSelected('sam2');
+//         comp.isSelected('sample_2');
 //       });
 //     });
 
 //     describe('changeValue', () => {
 //       it('should be change the value', () => {
 //         comp.facets = mockRes.facets;
-//         comp.changeValue('sam2');
+//         comp.changeValue('sample_2');
 //         comp.facets = ' ';
 //       });
 //       it('should be change the value of index', () => {
 //         comp.facets = mockRes.facets;
-//         comp.changeValue('sam4');
+//         comp.changeValue('sample_4');
 
 //         comp.facets = mockRes.facetsWithoutSelectedValue;
-//         comp.changeValue('sam4');
+//         comp.changeValue('sample_4');
 //       });
 //     });
