44b155e5167cd8d87c41fd5e198a72a405a7d757
Vivek Kasture <vivek_kasture@techjoomla.com>
2018-11-05 18:57:10 +0530

    Issue #SB-8825 feat: Added absolute path support in sunbird-mobile

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,6 +1,19 @@
-var useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+var { dev, prod } = require('@ionic/app-scripts/config/webpack.config.js');
+
+const path = require('path');
+const webpackMerge = require('webpack-merge');
+
+const customConfig = {
+  resolve: {
+    alias: {
+      '@app': path.resolve('src'),
+    }
+  }
+};
 
 module.exports = function () {
-  useDefaultConfig.prod.devtool = 'source-map';
-  return useDefaultConfig;
+  dev = webpackMerge(dev, customConfig);
+  prod = webpackMerge(prod, customConfig);
+  prod.devtool = 'source-map';
+  return { dev, prod };
 }
