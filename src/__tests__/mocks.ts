fe90ba865022a51b64185b500082d8961659ca2b
ankur01oct <ankur01oct@gmail.com>
2018-12-10 15:08:09 +0530

    Issue SB-9004 test: Unit test jest for user-report.ts

--- src/__tests__/mocks.ts
+++ src/__tests__/mocks.ts
@@ -35,71 +35,155 @@ const createSpyObj: <T extends {}>(methodNames: string[]) => Mockify<T>  = (meth
 };
 
 export const courseServiceMock = createSpyObj<CourseService>([
-  'getEnrolledCourses', 'enrollCourse', 'updateContentState', 'getCourseBatches',
-  'getBatchDetails', 'getContentState']);
-
-export const navCtrlMock = createSpyObj<NavController>(['pop', 'push']);
-
-export const navParamsMock = createSpyObj<NavParams>(['get']);
-
-export const zoneMock = createSpyObj<NgZone>([
-  'run']);
-
-export const authServiceMock = createSpyObj<AuthService>(['getSessionData']);
-
-export const commonUtilServiceMock = createSpyObj<CommonUtilService>([
-  'translateMessage', 'showMessage', 'showToast', 'getLoader'
+  'getEnrolledCourses',
+  'enrollCourse',
+  'updateContentState',
+  'getCourseBatches',
+  'getBatchDetails',
+  'getContentState'
 ]);
 
-export const eventsMock = createSpyObj<Events>(['publish']);
+export const navCtrlMock = createSpyObj<NavController>([
+  'pop',
+  'push',
+  'setRoot'
+]);
 
-export const contentServiceMock = createSpyObj<ContentService>(['getContentDetail', 'getLocalContents']);
+export const navParamsMock = createSpyObj<NavParams>([
+  'get'
+]);
 
-export const popoverCtrlMock = createSpyObj<PopoverController>(['create', 'present']);
+export const zoneMock = createSpyObj<NgZone>([
+  'run'
+]);
 
-export const fileUtilMock = createSpyObj<FileUtil>([]);
+export const oAuthServiceMock = createSpyObj<OAuthService>([
+  'doOAuthStepOne'
+]);
 
-export const platformMock = createSpyObj<Platform>(['registerBackButtonAction', 'exitApp']);
+export const containerServiceMock = createSpyObj<ContainerService>([
+]);
 
-export const translateServiceMock = createSpyObj<TranslateService>(['use',
-'get']);
+export const userProfileServiceMock = createSpyObj<UserProfileService>([
+]);
 
-export const socialSharingMock = createSpyObj<SocialSharing>([]);
+export const profileServiceMock = createSpyObj<ProfileService>([
+]);
 
-export const shareUtilMock = createSpyObj<ShareUtil>([]);
+export const authServiceMock = createSpyObj<AuthService>([
+  'getSessionData'
+]);
 
-export const buildParamServiceMock = createSpyObj<BuildParamService>(['getBuildConfigParam']);
+export const commonUtilServiceMock = createSpyObj<CommonUtilService>([
+  'getAppDirection',
+  'translateMessage',
+  'showMessage',
+  'showToast',
+  'getLoader'
+]);
+
+export const eventsMock = createSpyObj<Events>([
+  'publish',
+  'subscribe',
+  'unsubscribe'
+]);
+
+export const contentServiceMock = createSpyObj<ContentService>([
+  'getContentDetail',
+  'getLocalContents',
+  'importContent',
+  'getChildContents',
+  'cancelDownload'
+]);
+
+export const popoverCtrlMock = createSpyObj<PopoverController>([
+  'create',
+  'present']);
+
+export const fileUtilMock = createSpyObj<FileUtil>([
+  'internalStoragePath'
+]);
+
+export const platformMock = createSpyObj<Platform>([
+  'registerBackButtonAction',
+  'exitApp'
+]);
+
+export const translateServiceMock = createSpyObj<TranslateService>([
+  'use',
+  'get'
+]);
+
+export const socialSharingMock = createSpyObj<SocialSharing>([
+  'share'
+]);
+
+export const shareUtilMock = createSpyObj<ShareUtil>([
+  'exportEcar'
+]);
+
+export const buildParamServiceMock = createSpyObj<BuildParamService>([
+  'getBuildConfigParam'
+]);
 
 export const appGlobalServiceMock = createSpyObj<AppGlobalService>([
-  'isUserLoggedIn', 'getGuestUserInfo', 'generateConfigInteractEvent', 'openPopover', 'setEnrolledCourseList',
-  'getSessionData', 'getCurrentUser', 'getNameForCodeInFramework', 'getGuestUserType'
+  'isUserLoggedIn',
+  'getGuestUserInfo',
+  'generateConfigInteractEvent',
+  'openPopover',
+  'setEnrolledCourseList',
+  'getSessionData',
+  'getCurrentUser',
+  'getNameForCodeInFramework',
+  'getGuestUserType'
 ]);
 
 export const telemetryGeneratorServiceMock = createSpyObj<TelemetryGeneratorService>([
-  'generateStartTelemetry', 'generateImpressionTelemetry', 'generateSpineLoadingTelemetry',
-  'generateCancelDownloadTelemetry', 'generateInteractTelemetry', 'generateEndTelemetry',
-  'generatePageViewTelemetry'
+  'generateStartTelemetry',
+  'generateImpressionTelemetry',
+  'generateSpineLoadingTelemetry',
+  'generateCancelDownloadTelemetry',
+  'generateInteractTelemetry',
+  'generateEndTelemetry',
+  'generatePageViewTelemetry',
+  'generateBackClickedTelemetry'
 ]);
 
-export const courseUtilServiceMock = createSpyObj<CourseUtilService>(['showCredits',
-  'getImportContentRequestBody']);
+export const courseUtilServiceMock = createSpyObj<CourseUtilService>([
+  'showCredits',
+  'getImportContentRequestBody'
+]);
 
-export const appVersionMock = createSpyObj<AppVersion>(['getAppName']);
+export const appVersionMock = createSpyObj<AppVersion>([
+  'getAppName'
+]);
 
-export const pageAssembleServiceMock = createSpyObj<PageAssembleService>(['getPageAssemble']);
+export const pageAssembleServiceMock = createSpyObj<PageAssembleService>([
+  'getPageAssemble'
+]);
 
-export const sunbirdQRScannerMock = createSpyObj<SunbirdQRScanner>(['startScanner']);
+export const sunbirdQRScannerMock = createSpyObj<SunbirdQRScanner>([
+  'startScanner'
+]);
 
-export const sharedPreferencesMock = createSpyObj<SharedPreferences>(['getString', 'putString']);
+export const sharedPreferencesMock = createSpyObj<SharedPreferences>([
+  'getString',
+  'putString'
+]);
 
 export const formAndFrameworkUtilServiceMock = createSpyObj<FormAndFrameworkUtilService>([
   'getCourseFilterConfig'
 ]);
 
-export const loadingControllerMock = createSpyObj<LoadingController>(['create', 'present']);
+export const loadingControllerMock = createSpyObj<LoadingController>([
+  'create',
+  'present'
+]);
 
-export const reportServiceMock = createSpyObj<ReportService>(['getListOfReports',
-'getDetailReport']);
+export const reportServiceMock = createSpyObj<ReportService>([
+  'getListOfReports',
+  'getDetailReport'
+]);
 
 export const transferMock = createSpyObj<FileTransfer>(['create']);
 export const fileMock = createSpyObj<File>(['writeFile']);
