6097475c0af93f6fc2fd64ccbe3cb5b8f4ec9eb4
arafath <arafath.ahmed@goodworklabs.com>
2018-12-13 12:20:34 +0530

    Issue #SB-9004 test: Refactored tests for ConfirmAlertComponent

--- src/component/confirm-alert/confirm-alert.component.spec.ts
+++ src/component/confirm-alert/confirm-alert.component.spec.ts
@@ -1,65 +1,47 @@
-import { ConfirmAlertComponent } from './confirm-alert';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ViewController } from 'ionic-angular';
-import { Platform } from 'ionic-angular';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { TranslateModule } from '@ngx-translate/core';
-import { PlatformMock } from '../../../test-config/mocks-ionic';
+import {ConfirmAlertComponent} from './confirm-alert';
+import {platformMock, viewControllerMock} from '@app/__tests__/mocks';
 
-describe('Confirm-alert.component', () => {
-    let comp: ConfirmAlertComponent;
-    let fixture: ComponentFixture<ConfirmAlertComponent>;
+describe('Confirm-alert.confirmAlertComponentonent', () => {
+  let confirmAlertComponent: ConfirmAlertComponent;
 
-    beforeEach(() => {
-        const viewControllerStub = {
-            dismiss: () => ({})
-        };
-        TestBed.configureTestingModule({
-            imports: [TranslateModule.forRoot()],
-            declarations: [ConfirmAlertComponent],
-            schemas: [NO_ERRORS_SCHEMA],
-            providers: [
-                { provide: ViewController, useValue: viewControllerStub },
-                { provide: Platform, useClass: PlatformMock }
-            ]
-        });
+  beforeEach(() => {
+    confirmAlertComponent = new ConfirmAlertComponent(
+      viewControllerMock as any,
+      platformMock as any
+    );
 
-        const viewControllerMock = TestBed.get(ViewController);
-        const platformStub = TestBed.get(Platform);
-        spyOn(viewControllerMock, 'dismiss');
-        spyOn(platformStub, 'registerBackButtonAction');
-        // ConfirmAlertComponent.prototype.backButtonFunc = jasmine.any(Function);
-        // sspyOn(ConfirmAlertComponent.prototype, 'backButtonFunc').and.returnValue(jasmine.any(Function));
-        fixture = TestBed.createComponent(ConfirmAlertComponent);
-        comp = fixture.componentInstance;
-    });
+    jest.resetAllMocks();
+  });
 
-    it('should create instance of the ConfirmAlertComponent', () => {
-        expect(comp).toBeTruthy();
+  it('should create instance of the ConfirmAlertComponent', () => {
+    expect(confirmAlertComponent).toBeTruthy();
+  });
 
-        // expect(comp.backButtonFunc).toEqual(jasmine.any(Function));
-        // expect(comp.backButtonFunc).toBeUndefined();
-    });
+  it('should dismiss view on backButton', () => {
+    // arrange
+    platformMock.registerBackButtonAction.mockReturnValue(jest.fn());
 
-    it('should dismiss this popup and it should pass argument as false by default', () => {
-        expect(comp.selectOption).toBeDefined();
-        spyOn(comp, 'selectOption').and.callThrough();
+    confirmAlertComponent = new ConfirmAlertComponent(
+      viewControllerMock as any,
+      platformMock as any
+    );
 
-        const viewControllerMock = TestBed.get(ViewController);
-        comp.selectOption();
-        expect(comp.selectOption).toHaveBeenCalled();
-        expect(viewControllerMock.dismiss).toHaveBeenCalled();
-        expect(viewControllerMock.dismiss).toHaveBeenCalledWith(false);
-    });
+    // act
+    platformMock.registerBackButtonAction.mock.calls[0][0].call(confirmAlertComponent);
 
-    it('should dismiss this popup and it should pass argument which is passed to the selectOption', () => {
-        expect(comp.selectOption).toBeDefined();
-        spyOn(comp, 'selectOption').and.callThrough();
+    // assert
+    expect(viewControllerMock.dismiss).toHaveBeenCalled();
+    expect(confirmAlertComponent.backButtonFunc).toHaveBeenCalled();
+  });
 
-        const viewControllerMock = TestBed.get(ViewController);
-        comp.selectOption(true);
-        expect(comp.selectOption).toHaveBeenCalled();
-        expect(viewControllerMock.dismiss).toHaveBeenCalled();
-        expect(viewControllerMock.dismiss).toHaveBeenCalledWith(true);
-    });
+  it('should dismiss this popup and it should pass argument as false by default', () => {
+    expect(confirmAlertComponent.selectOption).toBeDefined();
+
+    // act
+    confirmAlertComponent.selectOption();
+
+    // assert
+    expect(viewControllerMock.dismiss).toHaveBeenCalled();
+    expect(viewControllerMock.dismiss).toHaveBeenCalledWith(false);
+  });
 });
