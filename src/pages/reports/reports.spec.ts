1d3cb4f3ffe0f1cd9a08cf287f0f99fc9e6d9f3c
Raghavendra Shivhare <shivhareraghavendra@gmail.com>
2019-01-16 12:51:22 +0530

    Issue #SB-9004 test: reports test cases completed t0 100% coverage

--- src/pages/reports/reports.spec.ts
+++ src/pages/reports/reports.spec.ts
@@ -13,8 +13,13 @@ import { ReportListPage } from './report-list/report-list';
 
 describe('ReportsPage test cases!', () => {
     let reports: ReportsPage;
+    const profileData: any = {
+        id: '8123891738'
+    };
 
     beforeEach(() => {
+        navParamsMock.get.mockReturnValue(profileData);
+
         reports = new ReportsPage(
             navCtrlMock as any,
             profileServiceMock as any,
@@ -28,25 +33,40 @@ describe('ReportsPage test cases!', () => {
         jest.resetAllMocks();
     });
 
-    it('', () => {
+    it('#ionViewDidLoad should call populateGroups() and dismiss()', (done) => {
         const loader = {
             present: jest.fn(),
             dismiss: jest.fn()
         };
-        spyOn(reports, 'populateUsers').and.returnValue(new Promise(resolved => {
-            return resolved;
-        }));
-        spyOn(reports, 'populateGroups');
+        spyOn(reports, 'populateGroups').and.returnValue(Promise.resolve({}));
+        spyOn(reports, 'populateUsers').and.returnValue(Promise.resolve([]));
         loadingControllerMock.create.mockReturnValue(loader);
         reports.ionViewDidLoad();
         expect(reports.populateUsers).toHaveBeenCalled();
-        expect(reports.populateGroups).toBeCalled();
+        setTimeout(() => {
+            expect(reports.populateGroups).toBeCalled();
+            zoneMock.run.mock.calls[0][0].call();
+            expect(loader.dismiss).toBeCalled();
+            done();
+        }, 0);
+    });
+
+    it('#ionViewDidLoad should go to catch and call dismiss()', (done) => {
+        const loader = {
+            present: jest.fn(),
+            dismiss: jest.fn()
+        };
+        spyOn(reports, 'populateUsers').and.returnValue(Promise.reject([]));
+        loadingControllerMock.create.mockReturnValue(loader);
+        reports.ionViewDidLoad();
+        setTimeout(() => {
+            expect(loader.dismiss).toBeCalled();
+            done();
+        }, 0);
     });
 
     it('#populateGroups should call getAllGroup() and resolve with result', (done) => {
-        const data = {
-            resuly: 'test'
-        };
+        const data = {};
         groupServiceMock.getAllGroup.mockResolvedValue(data);
         reports.populateGroups().then(() => {
             expect(groupServiceMock.getAllGroup).toBeCalled();
@@ -54,42 +74,48 @@ describe('ReportsPage test cases!', () => {
         });
     });
 
-    it('#populateUsers should call getAllUserProfile()  and getCurrentUser()', (done) => {
-        profileServiceMock.getAllUserProfile.mockResolvedValue(true);
-        profileServiceMock.getCurrentUser.mockResolvedValue(true);
-        spyOn(reports, 'filterOutCurrentUser').and.stub();
-        reports.populateUsers().then(() => {
-            expect(profileServiceMock.getAllUserProfile).toBeCalled();
+    it('#populateGroups should call getAllGroup() and group.result should be defined', (done) => {
+        const data = {
+            result: 'test'
+        };
+        groupServiceMock.getAllGroup.mockResolvedValue(data);
+        reports.populateGroups().then(() => {
+            expect(groupServiceMock.getAllGroup).toBeCalled();
+            expect(data.result).toBeDefined();
             done();
         });
     });
 
-    // it('#populateUsers should call catch() of getAllUserProfile', (done) => {
-    //     profileServiceMock.getAllUserProfile.mockRejectedValue(false);
-    //     // profileServiceMock.getCurrentUser.mockResolvedValue(true);
-    //     // spyOn(reports, 'filterOutCurrentUser').and.stub();
-    //     reports.populateUsers().then(() => {
-    //         expect(profileServiceMock.getCurrentUser).not.toBeCalled();
-    //         done();
-    //     });
-    // });
+    it('#populateUsers should call getAllUserProfile()  and getCurrentUser()', (done) => {
+        profileServiceMock.getAllUserProfile.mockResolvedValue(true);
+        profileServiceMock.getCurrentUser.mockResolvedValue(JSON.stringify({
+            uid: '8123891738'
+        }));
+        spyOn(reports, 'filterOutCurrentUser').and.stub();
+        reports.populateUsers().then(() => {
+            expect(profileServiceMock.getAllUserProfile).toBeCalled();
+            expect(reports.filterOutCurrentUser).toBeCalled();
+            done();
+        });
+    });
 
-    // it('#populateUsers should call catch() of getAllUserProfile', (done) => {
-    //     profileServiceMock.getAllUserProfile.mockRejectedValue(true);
-    //     reports.populateUsers().then(() => {
-    //         done();
-    //     });
-    // });
+    it('#populateUsers should call catch() of getAllUserProfile', (done) => {
+        profileServiceMock.getAllUserProfile.mockRejectedValue(false);
+        reports.populateUsers().catch(() => {
+            expect(profileServiceMock.getCurrentUser).not.toBeCalled();
+            done();
+        });
+    });
 
-    // it('#populateUsers should call catch() of getCurrentUser', (done) => {
-    //     profileServiceMock.getAllUserProfile.mockResolvedValue(true);
-    //     profileServiceMock.getCurrentUser.mockRejectedValue(true);
-    //     spyOn(reports, 'filterOutCurrentUser').and.stub();
-    //     reports.populateUsers().then(() => {
-    //         // expect(reports.filterOutCurrentUser).not.toBeCalled();
-    //         done();
-    //     });
-    // });
+    it('#populateUsers should call catch() of getCurrentUser', (done) => {
+        profileServiceMock.getAllUserProfile.mockResolvedValue(true);
+        profileServiceMock.getCurrentUser.mockRejectedValue(true);
+        spyOn(reports, 'filterOutCurrentUser').and.stub();
+        reports.populateUsers().catch(() => {
+            expect(reports.filterOutCurrentUser).not.toBeCalled();
+            done();
+        });
+    });
 
     it('#goToUserReportList should call push()', () => {
         reports.goToUserReportList('uid', 'handle');
@@ -115,5 +141,10 @@ describe('ReportsPage test cases!', () => {
         reports.onSegmentChange({});
         expect(telemetryGeneratorServiceMock.generateInteractTelemetry).toHaveBeenCalled();
         expect(telemetryGeneratorServiceMock.generateImpressionTelemetry).toHaveBeenCalled();
+    });
+
+    it('#filterOutCurrentUser should return a function', () => {
+        const returnValue = reports.filterOutCurrentUser([ 'Sam', 'Daisy'], { uid: '8123891738'});
+        expect(returnValue).toBeDefined();
     });
 });
