7b7e8c8f0458e47698ae8c6d85fa189b4c4df477
Anil Gupta <ani2824@gmail.com>
2019-02-21 20:54:52 +0530

    Issue #SB-10715 fix: Fixed appIcon loading.

--- src/pages/resources/resources.ts
+++ src/pages/resources/resources.ts
@@ -306,10 +306,16 @@ export class ResourcesPage implements OnInit, AfterViewInit {
       .then(data => {
         _.forEach(data, (value) => {
           value.contentData.lastUpdatedOn = value.lastUpdatedTime;
-          if (Boolean(value.isAvailableLocally) && value.basePath && value.contentData.appIcon) {
-            value.contentData.appIcon = value.basePath + '/' + value.contentData.appIcon;
-          } else if (!Boolean(value.isAvailableLocally)) {
-            value.contentData.appIcon = value.contentData.appIcon;
+          if (value.contentData.appIcon) {
+            if (Boolean(value.isAvailableLocally) && value.basePath) {
+              value.contentData.appIcon = value.basePath + '/' + value.contentData.appIcon;
+            } else if (!Boolean(value.isAvailableLocally)) {
+              if (value.contentData.appIcon.includes('http:') || value.contentData.appIcon.includes('https:')) {
+                value.contentData.appIcon = value.contentData.appIcon;
+              } else if (value.basePath) {
+                value.contentData.appIcon = value.basePath + '/' + value.contentData.appIcon;
+              }
+            }
           }
         });
         this.ngZone.run(() => {
