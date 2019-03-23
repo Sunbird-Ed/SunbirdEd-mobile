20fb2fbc22a79b5fd6b6c3d15836312df33fb3aa
sharath <sharaths.kashyap@gmail.com>
2019-03-23 12:46:46 +0530

    Issue #0000 chore:Type script downgrade due to webpack issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -162,7 +162,7 @@ export function sdkDriverFactory() {
 ];
 }
 
-export const sunbirdSdkServicesProvidersFactory = sdkDriverFactory;
+export const sunbirdSdkServicesProvidersFactory: () => Provider[] = sdkDriverFactory;
 
 export const sunbirdSdkFactory =
   () => {
