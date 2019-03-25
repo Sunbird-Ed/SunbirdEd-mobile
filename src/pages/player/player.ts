105ff3abeed180e31ef65f9048720e3ba17c0e8f
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-03-25 18:21:59 +0530

    Issue #SB-9994 feat: Hide Multiple pop on back button press

--- src/pages/player/player.ts
+++ src/pages/player/player.ts
@@ -2,15 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { CanvasPlayerService } from './canvas-player.service';
 import { ScreenOrientation } from '@ionic-native/screen-orientation';
-
-
-/**
- * Generated class for the PlayerPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import { IonicApp } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -18,7 +10,6 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation';
   templateUrl: 'player.html',
 })
 export class PlayerPage {
-  private win: any = window;
   unregisterBackButton: any;
   config = {};
   constructor(
@@ -26,48 +17,46 @@ export class PlayerPage {
     public navParams: NavParams,
     private canvasPlayerService: CanvasPlayerService,
     private platform: Platform,
-    private screenOrientation: ScreenOrientation  
+    private screenOrientation: ScreenOrientation,
+    private ionicApp: IonicApp
   ) {
     this.canvasPlayerService.handleAction();
-    this.screenOrientation.lock('landscape');
   }
 
   ionViewWillEnter() {
     this.unregisterBackButton = this.platform.registerBackButtonAction(() => {
-      this.canvasPlayerService.showConfirm();
+      if (!this.ionicApp._overlayPortal.getActive())
+        this.canvasPlayerService.showConfirm();
     }, 11);
 
-
-    let that = this;
     this.config = this.navParams.get('config');
-    
+
     let previewElement: HTMLIFrameElement = document.getElementById('preview') as HTMLIFrameElement;
-    //previewElement.src = 'build/content-player/preview.html?date=' + new Date().toLocaleString();
-    that.config['context'].hierarchyInfo = that.config['metaData'].hierarchyInfo;
-    that.config['metaData'].contentData.basePath = that.config['metaData'].basePath.replace(/\/$/, "");
-    that.config['metaData'].contentData.basepath = that.config['metaData'].basePath.replace(/\/$/, "");      
-    that.config['metadata'] = that.config['metaData'];
-    that.config['uid'] = that.config['context'].actor.id;
-    delete that.config['metaData'];
+    this.config['context'].hierarchyInfo = this.config['metaData'].hierarchyInfo;
+    this.config['metaData'].contentData.basePath = this.config['metaData'].basePath.replace(/\/$/, "");
+    this.config['metaData'].contentData.basepath = this.config['metaData'].basePath.replace(/\/$/, "");
+    this.config['metadata'] = this.config['metaData'];
+    this.config['uid'] = this.config['context'].actor.id;
+    delete this.config['metaData'];
     previewElement.contentWindow.location.reload();
-    previewElement.onload = function () {
-      
-      console.log("config", that.config);
+    previewElement.onload = () => {
+
+      console.log("config", this.config);
       setTimeout(() => {
-        (previewElement['contentWindow'] as any).initializePreview(that.config);
-      }, 100);
+        (previewElement['contentWindow'] as any).initializePreview(this.config);
+      }, 1000);
     }
   }
 
+  ionViewDidEnter() {
+    this.screenOrientation.lock('landscape');
+  }
   getConfiguration() {
     var mobileConfig = this.navParams.get('config');
-    // mobileConfig.config.metaData.basePath = mobileConfig.config.metaData.basePath.replace(/\/$/, "");
-    // build/content-player/preview.htmlmobileConfig.config.metaData.basepath = mobileConfig.config.metaData.basePath;
-
     var config = {
       "context": mobileConfig.context,
-      "config": mobileConfig.config, 
-      "metadata": mobileConfig.metaData, 
+      "config": mobileConfig.config,
+      "metadata": mobileConfig.metaData,
       "data": mobileConfig.data
     }
     return config;
