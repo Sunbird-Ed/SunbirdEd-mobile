12683ca97a35349347187903caee31ef7d4e9b39
arafath <arafath.ahmed@goodworklabs.com>
2019-03-29 18:22:25 +0530

    Issue #SB-11405 FIX To add markImportDone() method typing for splashcreen plugin

--- typings/cordova-plugin-sunbirdsplash.d.ts
+++ typings/cordova-plugin-sunbirdsplash.d.ts
@@ -2,4 +2,5 @@ declare var splashscreen: {
   hide: () => void;
   setImportProgress: (currentCount, totalCount) => void;
   getActions: (successCallback: (actions: string) => void) => void;
+  markImportDone: () => void;
 };
