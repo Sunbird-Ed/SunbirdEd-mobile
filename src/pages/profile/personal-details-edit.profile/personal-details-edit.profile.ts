23083dea8ea8e6397037101de229c4dd337aba1b
balakrishna@mantralabsglobal.com <balakrishna10@gmail.com>
2019-04-02 16:43:48 +0530

    Issue #SB-11499 fix: Profile: Unable to edit the Name in the regional language

--- src/pages/profile/personal-details-edit.profile/personal-details-edit.profile.ts
+++ src/pages/profile/personal-details-edit.profile/personal-details-edit.profile.ts
@@ -87,7 +87,7 @@ export class PersonalDetailsEditPage {
     this.profileEditForm = this.fb.group({
       states: userState || [],
       districts: userDistrict || [],
-      name: [profilename.trim()],
+      name: [profilename.trim(), Validators.required],
     });
     this.enableSubmitButton();
   }
