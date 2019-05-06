052727e9b639ec4518e39affc882227e4cae63ef
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2019-05-06 16:11:13 +0530

    Issue #SB-11096 feat: Smoothening hide and show header-footers

--- src/directives/hide-tab-footer/hide-tab-footer.ts
+++ src/directives/hide-tab-footer/hide-tab-footer.ts
@@ -1,12 +1,6 @@
 import { Directive, Renderer2, ElementRef } from '@angular/core';
 import { Events } from 'ionic-angular';
 
-/**
- * Generated class for the HideTabFooterDirective directive.
- *
- * See https://angular.io/api/core/Directive for more info on Angular
- * Directives.
- */
 @Directive({
   selector: '[hide-tab-footer]' // Attribute selector
 })
@@ -14,21 +8,9 @@ export class HideTabFooterDirective {
 
   hideTimer: NodeJS.Timer;
 
-  constructor(private elemRef: ElementRef, private renderer: Renderer2, public event: Events) {
-    console.log('Hello HideTabFooterDirective Directive');
-  }
+  constructor(private elemRef: ElementRef, private renderer: Renderer2, public event: Events) {}
 
   ngOnInit() {
-    console.log(this.elemRef.nativeElement);
-    console.log(document.querySelector('.tabbar'));
-    // this.renderer.setStyle(this.elemRef.nativeElement.querySelector('.tabbar'), 'webkitTransition', 'bottom 300ms');
-    // this.event.subscribe('hideFooterTabOnScroll', (data) => {
-    //   console.log('hideFooterTabOnScroll');
-    //   this.renderer.setStyle(this.elemRef.nativeElement.querySelector('.tabbar'), 'bottom', '-56px');
-    //   this.hideTimer = setTimeout(() => {
-    //     this.renderer.setStyle(this.elemRef.nativeElement.querySelector('.tabbar'), 'bottom', '0px');
-    //   }, 1000);
-    // });
     this.event.subscribe('hideFooterTabOnScroll', (data) => {
       console.log('hideFooterTabOnScroll');
       this.renderer.setStyle(document.querySelector('.tabbar'), 'webkitTransition', 'bottom 500ms');
