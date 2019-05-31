c871ecf252c2ab01eb5cd4a15730f5e4a7f73626
ankur01oct <ankur01oct@gmail.com>
2019-05-31 17:14:14 +0530

    Issue SB-12805 feat: permission settings implementation

--- src/component/popups/sb-popover/sb-popover.ts
+++ src/component/popups/sb-popover/sb-popover.ts
@@ -41,10 +41,13 @@ export class SbPopoverComponent {
   showFlagMenu = true;
   public objRollup: Rollup;
   private corRelationList: Array<CorrelationData>;
-
+  isNotShowCloseIcon: boolean;
+  img: any;
 
   constructor(public viewCtrl: ViewController, public navParams: NavParams,
     private platform: Platform, private events: Events) {
+    this.img = this.navParams.get('img');
+    this.isNotShowCloseIcon = this.navParams.get('isNotShowCloseIcon') ? true : false;
     this.content = this.navParams.get('content');
     this.actionsButtons = this.navParams.get('actionsButtons');
     this.icon = this.navParams.get('icon');
@@ -59,7 +62,7 @@ export class SbPopoverComponent {
     this.pageName = this.navParams.get('pageName');
     this.objRollup = this.navParams.get('objRollup');
     this.corRelationList = this.navParams.get('corRelationList');
-
+    
     if (this.navParams.get('isChild')) {
       this.isChild = true;
     }
