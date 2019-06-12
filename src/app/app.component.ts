0dad1e498e5d6c53cfb9b4bda56fadf6f4393725
raghav14 <shivhareraghavendra@gmail.com>
2019-06-12 12:38:03 +0530

    Issue #SB-12971 feat: code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -194,14 +194,11 @@ export class MyApp implements OnInit, AfterViewInit {
     FCMPlugin.onNotification((data) => {
       console.log('Notificationdata');
       console.log(data);
-      alert(data);
       this.notificationServices.addNotification(data).subscribe();
         if (data.wasTapped) {
           // Notification was received on device tray and tapped by the user.
-          alert( JSON.stringify(data) );
         } else {
           // Notification was received in foreground. Maybe the user needs to be notified.
-          alert( JSON.stringify(data) );
         }
     },
     (sucess) => {
