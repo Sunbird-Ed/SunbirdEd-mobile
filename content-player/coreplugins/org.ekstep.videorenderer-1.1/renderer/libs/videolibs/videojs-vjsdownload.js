9d36553fd335138a130980a593396f24b939aae0
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-06-18 18:47:07 +0530

    Issue #SB-0000 feat: Updated Player build

--- content-player/coreplugins/org.ekstep.videorenderer-1.1/renderer/libs/videolibs/videojs-vjsdownload.js
+++ content-player/coreplugins/org.ekstep.videorenderer-1.1/renderer/libs/videolibs/videojs-vjsdownload.js
@@ -1,127 +1,133 @@
 /**
  * videojs-vjsdownload
- * @version 1.0.3
- * @copyright 2018 7Ds7
+ * @version 1.0.4
+ * @copyright 2019 7Ds7
  * @license Apache-2.0
  */
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.videojsVjsdownload = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (global){
-'use strict';
-
-Object.defineProperty(exports, '__esModule', {
-  value: true
-});
-
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var _videoJs = (typeof window !== "undefined" ? window['videojs'] : typeof global !== "undefined" ? global['videojs'] : null);
-
-var _videoJs2 = _interopRequireDefault(_videoJs);
-
-// Default options for the plugin.
-var defaults = {
-  beforeElement: 'fullscreenToggle',
-  textControl: 'Download video',
-  name: 'downloadButton'
-};
-
-var vjsButton = _videoJs2['default'].getComponent('Button');
-
-var DownloadButton = (function (_vjsButton) {
-  _inherits(DownloadButton, _vjsButton);
-
-  function DownloadButton() {
-    _classCallCheck(this, DownloadButton);
-
-    _get(Object.getPrototypeOf(DownloadButton.prototype), 'constructor', this).apply(this, arguments);
+! function(e) {
+  if ("object" == typeof exports && "undefined" != typeof module) module.exports = e();
+  else if ("function" == typeof define && define.amd) define([], e);
+  else {
+      var t;
+      t = "undefined" != typeof window ? window : "undefined" != typeof global ? global : "undefined" != typeof self ? self : this, t.videojsVjsdownload = e()
   }
+}(function() {
+  return function e(t, o, n) {
+      function r(l, u) {
+          if (!o[l]) {
+              if (!t[l]) {
+                  var f = "function" == typeof require && require;
+                  if (!u && f) return f(l, !0);
+                  if (i) return i(l, !0);
+                  var a = new Error("Cannot find module '" + l + "'");
+                  throw a.code = "MODULE_NOT_FOUND", a
+              }
+              var d = o[l] = {
+                  exports: {}
+              };
+              t[l][0].call(d.exports, function(e) {
+                  var o = t[l][1][e];
+                  return r(o || e)
+              }, d, d.exports, e, t, o, n)
+          }
+          return o[l].exports
+      }
+      for (var i = "function" == typeof require && require, l = 0; l < n.length; l++) r(n[l]);
+      return r
+  }({
+      1: [function(e, t, o) {
+          (function(e) {
+              "use strict";
 
-  /**
-   * Function to invoke when the player is ready.
-   *
-   * This is a great place for your plugin to initialize itself. When this
-   * function is called, the player will have its DOM and child components
-   * in place.
-   *
-   * @function onPlayerReady
-   * @param    {Player} player
-   * @param    {Object} [options={}]
-   */
+              function n(e, t) {
+                  if (!(e instanceof t)) throw new TypeError("Cannot call a class as a function")
+              }
 
-  _createClass(DownloadButton, [{
-    key: 'buildCSSClass',
-
-    /**
-    * Allow sub components to stack CSS class names
-    *
-    * @return {String} The constructed class name
-    * @method buildCSSClass
-    */
-    value: function buildCSSClass() {
-      return 'vjs-vjsdownload ' + _get(Object.getPrototypeOf(DownloadButton.prototype), 'buildCSSClass', this).call(this);
-    }
-
-    /**
-    * Handles click for full screen
-    *
-    * @method handleClick
-    */
-  }, {
-    key: 'handleClick',
-    value: function handleClick() {
-      var p = this.player();
-
-      window.open(p.currentSrc(), 'Download');
-      p.trigger('downloadvideo');
-    }
-  }]);
-
-  return DownloadButton;
-})(vjsButton);
-
-var onPlayerReady = function onPlayerReady(player, options) {
-  var DButton = player.controlBar.addChild(new DownloadButton(player, options), {});
-
-  DButton.controlText(options.textControl);
-
-  player.controlBar.el().insertBefore(DButton.el(), player.controlBar.getChild(options.beforeElement).el());
-
-  player.addClass('vjs-vjsdownload');
-};
-
-/**
- * A video.js plugin.
- *
- * In the plugin function, the value of `this` is a video.js `Player`
- * instance. You cannot rely on the player being in a "ready" state here,
- * depending on how the plugin is invoked. This may or may not be important
- * to you; if not, remove the wait for "ready"!
- *
- * @function vjsdownload
- * @param    {Object} [options={}]
- *           An object of options left to the plugin author to define.
- */
-var vjsdownload = function vjsdownload(options) {
-  var _this = this;
-
-  this.ready(function () {
-    onPlayerReady(_this, _videoJs2['default'].mergeOptions(defaults, options));
-  });
-};
-
-// Register the plugin with video.js.
-_videoJs2['default'].registerPlugin('vjsdownload', vjsdownload);
-
-exports['default'] = vjsdownload;
-module.exports = exports['default'];
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[1])(1)
+              function r(e, t) {
+                  if ("function" != typeof t && null !== t) throw new TypeError("Super expression must either be null or a function, not " + typeof t);
+                  e.prototype = Object.create(t && t.prototype, {
+                      constructor: {
+                          value: e,
+                          enumerable: !1,
+                          writable: !0,
+                          configurable: !0
+                      }
+                  }), t && (Object.setPrototypeOf ? Object.setPrototypeOf(e, t) : e.__proto__ = t)
+              }
+              Object.defineProperty(o, "__esModule", {
+                  value: !0
+              });
+              var i = function() {
+                      function e(e, t) {
+                          for (var o = 0; o < t.length; o++) {
+                              var n = t[o];
+                              n.enumerable = n.enumerable || !1, n.configurable = !0, "value" in n && (n.writable = !0), Object.defineProperty(e, n.key, n)
+                          }
+                      }
+                      return function(t, o, n) {
+                          return o && e(t.prototype, o), n && e(t, n), t
+                      }
+                  }(),
+                  l = function(e, t, o) {
+                      for (var n = !0; n;) {
+                          var r = e,
+                              i = t,
+                              l = o;
+                          n = !1, null === r && (r = Function.prototype);
+                          var u = Object.getOwnPropertyDescriptor(r, i);
+                          if (void 0 !== u) {
+                              if ("value" in u) return u.value;
+                              var f = u.get;
+                              if (void 0 === f) return;
+                              return f.call(l)
+                          }
+                          var a = Object.getPrototypeOf(r);
+                          if (null === a) return;
+                          e = a, t = i, o = l, n = !0, u = a = void 0
+                      }
+                  },
+                  u = "undefined" != typeof window ? window.videojs : void 0 !== e ? e.videojs : null,
+                  f = function(e) {
+                      return e && e.__esModule ? e : {
+                          default: e
+                      }
+                  }(u),
+                  a = {
+                      beforeElement: "fullscreenToggle",
+                      textControl: "Download video",
+                      name: "downloadButton",
+                      downloadURL: null
+                  },
+                  d = f.default.getComponent("Button"),
+                  c = function(e) {
+                      function t() {
+                          n(this, t), l(Object.getPrototypeOf(t.prototype), "constructor", this).apply(this, arguments)
+                      }
+                      return r(t, e), i(t, [{
+                          key: "buildCSSClass",
+                          value: function() {
+                              return "vjs-vjsdownload " + l(Object.getPrototypeOf(t.prototype), "buildCSSClass", this).call(this)
+                          }
+                      }, {
+                          key: "handleClick",
+                          value: function() {
+                              var e = this.player();
+                              window.open(this.options_.downloadURL || e.currentSrc(), "Download"), e.trigger("downloadvideo")
+                          }
+                      }]), t
+                  }(d),
+                  s = function(e, t) {
+                      var o = e.controlBar.addChild(new c(e, t), {});
+                      o.controlText(t.textControl), e.controlBar.el().insertBefore(o.el(), e.controlBar.getChild(t.beforeElement).el()), e.addClass("vjs-vjsdownload")
+                  },
+                  p = function(e) {
+                      var t = this;
+                      this.ready(function() {
+                          s(t, f.default.mergeOptions(a, e))
+                      })
+                  };
+              f.default.registerPlugin("vjsdownload", p), o.default = p, t.exports = o.default
+          }).call(this, "undefined" != typeof global ? global : "undefined" != typeof self ? self : "undefined" != typeof window ? window : {})
+      }, {}]
+  }, {}, [1])(1)
 });
