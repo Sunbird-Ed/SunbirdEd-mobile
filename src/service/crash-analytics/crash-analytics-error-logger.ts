af43dd7f9062e075b752538dffd3e89c9d20b862
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2019-06-24 20:05:52 +0530

    Issue #SB-12899 feat: removing error logger method, and integrating inside telemetry

--- src/service/crash-analytics/crash-analytics-error-logger.ts
+++ src/service/crash-analytics/crash-analytics-error-logger.ts
@@ -1,7 +1,7 @@
 import { ErrorHandler, Optional } from '@angular/core';
 import { ViewController, NavControllerBase, App } from 'ionic-angular';
 import { ActivePageService } from '@app/service/active-page/active-page-service';
-import { SunbirdSdk, TelemetryErrorRequest, ErrorStack } from 'sunbird-sdk';
+import { SunbirdSdk, TelemetryErrorRequest } from 'sunbird-sdk';
 
 export class CrashAnalyticsErrorLogger extends ErrorHandler {
 
@@ -14,7 +14,6 @@ export class CrashAnalyticsErrorLogger extends ErrorHandler {
     }
 
     handleError(error: Error | string | any): void {
-        console.log('ERROR', error);
         const telemetryErrorRequest: TelemetryErrorRequest = {
             errorCode: '',
             errorType: '',
@@ -22,30 +21,19 @@ export class CrashAnalyticsErrorLogger extends ErrorHandler {
             pageId: ''
         };
 
-        const errorLoggerRequest: ErrorStack = {
-            pageid: '',
-            log: ''
-        };
-
         if (error instanceof Error) {
             telemetryErrorRequest.stacktrace = error.stack.slice(0, 250); // 250 characters limited for Telemetry and API purpose.
             telemetryErrorRequest.errorType = error.name || '';
-
-            errorLoggerRequest.log = telemetryErrorRequest.stacktrace;
         }
 
         try {
             const navObj: NavControllerBase = this.app.getActiveNavs()[0];
             const activeView: ViewController = navObj.getActive();
             telemetryErrorRequest.pageId = this.activePageService.computePageId((<any>activeView).instance);
-            errorLoggerRequest.pageid = telemetryErrorRequest.pageId;
         } catch (e) { }
 
         if (SunbirdSdk.instance) {
-
             SunbirdSdk.instance.telemetryService.error(telemetryErrorRequest).toPromise();
-
-            // SunbirdSdk.instance.errorLoggerService.logError(errorLoggerRequest).toPromise();
         }
 
         super.handleError(error);
