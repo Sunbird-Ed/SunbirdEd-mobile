8fea46b5f7d41f9cfc4219f8275226fbfe752307
Raghavendra Shivhare <shivhareraghavendra@gmail.com>
2019-09-06 19:12:30 +0530

    Issue #SB-14656 fix: added telemetry for download certificates

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -460,6 +460,15 @@ export class ProfilePage implements OnInit, AfterViewInit {
   }
 
   downloadTrainingCertificate(course: Course, certificate: CourseCertificate) {
+    const telemetryObject: TelemetryObject  = new TelemetryObject(certificate.id, ContentType.CERTIFICATE, undefined);
+
+    this.telemetryGeneratorService.generateInteractTelemetry(InteractType.TOUCH,
+    InteractSubtype.DOWNLOAD_CERTIFICATE_CLICKED,
+    Environment.USER, // env
+    PageId.PROFILE, // page name
+    telemetryObject,
+    undefined);
+
     this.courseService.downloadCurrentProfileCourseCertificate({
       courseId: course.courseId,
       certificateToken: certificate.token
